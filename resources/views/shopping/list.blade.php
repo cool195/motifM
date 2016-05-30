d1562eaa7cd8cf014b2734083a0b3ba79c268bcc
JuChao <juchao@evermarker.net>
2016-05-30 10:56:27 +0800

    添加模版引入

--- resources/views/shopping/list.blade.php
+++ resources/views/shopping/list.blade.php
@@ -28,26 +28,7 @@
 <!--<div class="font-size-sm"><a href="#">DOWNLOAD</a></div>-->
 <!--</nav>-->
 <!-- 头部导航 -->
-<header class="navbar-fixed-top" id="header">
-    <nav class="navbar navbar-full bg-primary">
-        <ul class="nav navbar-primary">
-            <li class="nav-item nav-menu">
-                <div class="">
-                    <i class="nav-tap iconfont icon-hamburger icon-size-lg"></i>
-                </div>
-            </li>
-            <li class="nav-item nav-logo">
-                <a><img src="/images/logo/logo.png" srcset="/images/logo/logo@2x.png 2x,/images/logo/logo@3x.png 3x"></a>
-            </li>
-            <li class="nav-item">
-                <div class="nav-shoppingCart">
-                    <i class="nav-tap iconfont icon-shopbag icon-size-lg"></i>
-                    <span class="shoppingCart-number">2</span>
-                </div>
-            </li>
-        </ul>
-    </nav>
-</header>
+@include('navigator')
 <nav class="navbar-fixed-top swiper-container bg-gray" id="tabIndex-container">
     <ul class="nav nav-tabs swiper-wrapper">
         @foreach($categories as $key => $c)
@@ -75,74 +56,7 @@
     </div>
 </section>
 <!-- 页脚 功能链接 -->
-<footer class="p-y-20x">
-    <div class="field-content m-t-15x m-b-20x">
-        <div class="field-text font-size-sm">
-            Follow Us:&nbsp;
-        </div>
-        <div class="field-items">
-            <a class="share-btn">
-                <i class="iconfont icon-facebook icon-size-lg"></i>
-            </a>
-            <a class="share-btn">
-                <i class="iconfont icon-google icon-size-lg"></i>
-            </a>
-            <a class="share-btn">
-                <i class="iconfont icon-youtube icon-size-lg"></i>
-            </a>
-            <a class="share-btn">
-                <i class="iconfont icon-linkedin icon-size-lg"></i>
-            </a>
-            <a class="share-btn">
-                <i class="iconfont icon-twitter icon-size-lg"></i>
-            </a>
-        </div>
-    </div>
-    <div class="field-content">
-        <div class="field-text font-size-sm">
-            Download:
-        </div>
-        <div class="field-items">
-            <a href="#" class="btn btn-secondary btn-xs">
-                <img src="/images/icon/icon-appStore.png" srcset="/images/icon/icon-appStore@2x.png 2x, /images/icon/icon-appStore@3x.png 3x">
-            </a>
-            <a href="#" class="btn btn-secondary btn-xs">
-                <img src="/images/icon/icon-googlePlay.png" srcset="/images/icon/icon-googlePlay@2x.png 2x, /images/icon/icon-googlePlay@3x.png 3x">
-            </a>
-        </div>
-    </div>
-    <div class="links-group container-fluid p-x-0">
-        <hr class="hr-dark m-t-20x m-b-15x">
-        <div class="row">
-            <div class="col-xs-6">
-                <div class="list-group font-size-sm">
-                    <div class="list-group-item list-group-itemText-lg text-primary"><strong>THE MOTIF</strong>
-                    </div>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">About Motif</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Privacy Poilcy</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Terms of Services</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Contact Us</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Site Map</a>
-                </div>
-            </div>
-            <div class="col-xs-6">
-                <div class="foot-list-group font-size-sm">
-                    <div class="list-group-item list-group-itemText-lg text-primary"><strong>HELP & SERVICE</strong>
-                    </div>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">FAQs</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Feedback</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Delivery & Shipping</a>
-                    <a class="list-group-item list-group-itemText-lg text-primary" href="#">Returns</a>
-                </div>
-
-            </div>
-        </div>
-        <hr class="hr-dark m-t-20x m-b-15x">
-    </div>
-    <div class="text-common text-center font-size-sm">Copyright @ 2016 EverMarker Inc. All rights
-        reserved.
-    </div>
-</footer>
+@include('footer')
 </body>
 <!-- 模板 -->
 <template id="tpl-product">
