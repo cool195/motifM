cbde4feebb58886065e3bd3df3ea018099bd7227
zhangtao <250574087@qq.com>
2016-06-03 10:29:25 +0800

     列表页链接修改

--- resources/views/shopping/list.blade.php
+++ resources/views/shopping/list.blade.php
@@ -71,7 +71,7 @@
         <div class="productList-item">
             <div class="image-bg">
                 <div class="image-container">
-                    <a href="http://motiflumen.evermarker.com/products/@{{ $value.spu }}">
+                    <a href="/detail/@{{ $value.spu }}">
                         <img class="img-fluid img-lazy" data-original="https://s3-us-west-1.amazonaws.com/emimagetest/n4/@{{ $value.main_image_url }}" src="/images/product/bg-product.jpg" alt="@{{ $value.main_title }}">
                         @{{ if $value.skuPrice.sale_price !== $value.skuPrice.price }}
                         <div class="price-off">
