d38897b71a04921aba205a43782a8b805ad711a0
zhaozhe <zhaozhe@evermarker.net>
2016-06-07 17:36:51 +0800

    update 更新静态文件

---
+++ resources/static/dist/scripts/shoppingCart.js
@@ -0,0 +1,148 @@
+'use strict';
+/* global jQuery */
+
+(function ($) {
+    var options = {
+        closeOnOutsideClick: false,
+        hashTracking: true
+    };
+    // loading 打开
+    function openLoading() {
+        $('.loading').toggleClass('loading-hidden');
+        setTimeout(function () {
+            $('.loading').toggleClass('loading-open');
+        }, 25);
+    }
+
+    // loading 隐藏
+    function closeLoading() {
+        $('.loading').addClass('loading-close');
+        setTimeout(function () {
+            $('.loading').toggleClass('loading-hidden loading-open').removeClass('loading-close');
+        }, 500);
+    }
+
+    // 删除对应商品
+    // TODO loading , ajax返回后 , 刷新页面
+    function deleteCart() {
+        // 判断相应的参数里是否有相应的值
+        var sku = $('#cartDialog').data('sku'),
+            action = $('#cartDialog').data('action');
+        if (sku === undefined || action === undefined || sku === '' || action === '' || sku === null || action === null) {
+            console.log('sku||action 为空');
+            return;
+        }
+        openLoading();
+        $.ajax({
+            url: '/path/to/file',
+            type: 'DELETE',
+            data: { cmd: action, sku: sku }
+        }).done(function () {
+            console.log('success');
+        }).fail(function () {
+            console.log('error');
+        }).always(function () {
+            closeLoading();
+            console.log('complete');
+        });
+    }
+
+    // 移动商品 move to cart || move to save
+    /**
+     * moveProduct()
+     * @param action
+     * @param sku
+     */
+    // TODO loading , ajax返回后 , 刷新页面
+    function moveProduct(action, sku) {
+        if (sku === undefined || action === undefined || sku === '' || action === '' || sku === null || action === null) {
+            console.log('sku||action 为空');
+            return;
+        }
+        openLoading();
+        $.ajax({
+            url: '/path/to/file',
+            type: 'UPDATE',
+            data: { cmd: action, sku: sku }
+        }).done(function () {
+            console.log('success');
+        }).fail(function () {
+            console.log('error');
+        }).always(function () {
+            closeLoading();
+            console.log('complete');
+        });
+    }
+
+    // TODO 加载的 loading 动画
+    // 调整商品数量
+    function changeQtty(Sku, Qtty) {
+        openLoading();
+        $.ajax({
+            url: '/path/to/file',
+            type: 'UPDATE',
+            data: { sku: Sku, qtty: Qtty }
+        }).done(function () {
+            console.log('success');
+        }).fail(function () {
+            console.log('error');
+        }).always(function () {
+            closeLoading();
+            console.log('complete');
+        });
+    }
+
+    // 购物车对话框 触发点
+    $('[data-remodal-target="modal"]').click(function (e) {
+        // 暂存数据 to modal , 为拼接 ajax url 做准备
+        $('#cartDialog').data('sku', $(e.target).data('sku'));
+        $('#cartDialog').data('action', $(e.target).data('action'));
+        console.log('open');
+    });
+
+    // 初始化 模态框
+    $('#cartDialog').remodal(options);
+
+    $('#cartDialog').on('closed', function () {
+        $(this).removeData('sku').removeData('action');
+        console.log('close');
+    });
+    $('#cartDialog').on('confirmation', function () {
+        deleteCart();
+    });
+
+    // move 商品的出发点
+    $('[data-product-move]').on('click', function () {
+        var action = $(this).data('product-move');
+        var sku = $(this).data('sku');
+        moveProduct(action, sku);
+    });
+
+    // 更新商品数量
+    $('[data-item-qty]').on('click', function (e) {
+        var $QtyCount;
+        // 获取加减号的标签项
+        if ($(e.target)[0].tagName === 'I') {
+            $QtyCount = $(e.target).parents('.btn-cartCount');
+        } else {
+            $QtyCount = $(e.target);
+        }
+        // 标签是否 disabled
+        if ($QtyCount.hasClass('disabled')) {
+            return;
+        }
+        // Sku 从父级标签获取 SkuID
+        // Qtty 从兄弟标签获取 商品数量
+        var Sku = $QtyCount.parents('.btn-group').data('sku'),
+            Qtty = $QtyCount.siblings('[data-item-num]').data('item-num');
+        // 判断点击的项是 + 还是 -
+        if ($QtyCount.data('item-qty') === 'add') {
+            // 加号
+            Qtty++;
+        } else {
+            Qtty--;
+        }
+        changeQtty(Sku, Qtty);
+    });
+})(jQuery);
+//# sourceMappingURL=shoppingCart.js.map
--- resources/views/shopping/cart.blade.php
+++ resources/views/shopping/cart.blade.php
@@ -17,19 +17,19 @@
 
 </head>
 <body>
-    <!-- 外层容器 -->
-    <div id="body-content">
-	@include('nav')	
-        <div class="body-container">
-			@include('navigator')
+<!-- 外层容器 -->
+<div id="body-content">
+    @include('nav')
+    <div class="body-container">
+    @include('navigator')
 
-            <!-- 购物袋 商品列表 -->
-            <section class="p-b-20x">
-                <!-- "Shopping Bag" 标题 -->
-                <article class="font-size-md text-main p-a-10x"><strong>Shopping Bag</strong></article>
+    <!-- 购物袋 商品列表 -->
+        <section class="p-b-20x">
+            <!-- "Shopping Bag" 标题 -->
+            <article class="font-size-md text-main p-a-10x"><strong>Shopping Bag</strong></article>
 
-				@if(empty($cartData['showSkus']))
-                <!-- 空袋子 提示信息 -->
+        @if(empty($cartData['showSkus']))
+            <!-- 空袋子 提示信息 -->
                 <div class="shopbag-empty-content p-x-10x">
                     <div class="container shopbag-emptyInfo">
                         <div class="m-b-20x p-b-5x"><i class="btn-shopbagEmpty iconfont icon-shopbag"></i></div>
@@ -37,65 +37,74 @@
                         <a href="#" class="btn btn-primary btn-block btn-sm">Continue Shopping</a>
                     </div>
                 </div>
-				@else
-                <!-- 商品列表 -->
+        @else
+            <!-- 商品列表 -->
                 <section class="cartList bg-white">
-					@foreach($cartData['showSkus'] as $showSku)
-                    <div class="cartList-item p-a-10x @if(1 !== $showSku['stock_status']) disabled @endif">
-                        <div class="productInfo flex">
-                            <div class="flex-fixedShrink">
-                                <img class="img-thumbnail" src="{{ 'https://s3-us-west-1.amazonaws.com/emimagetest/n2/'.$showSku['main_image_url'] }}" width="70px" height="70px">
+                    @foreach($cartData['showSkus'] as $showSku)
+                        <div class="cartList-item p-a-10x @if(1 !== $showSku['stock_status']) disabled @endif">
+                            <div class="productInfo flex">
+                                <div class="flex-fixedShrink">
+                                    <img class="img-thumbnail"
+                                         src="{{ 'https://s3-us-west-1.amazonaws.com/emimagetest/n2/'.$showSku['main_image_url'] }}"
+                                         width="70px" height="70px">
+                                </div>
+                                <div class="p-l-10x flex-width">
+                                    <article class="flex flex-fullJustified">
+                                        <!--<h6 class="text-main font-size-md p-r-10x">
+                                               <strong>Crown Shape Black Gold-plated Sterling Silver Engagehape Black
+                                                       Gold-plated
+                                                       Sterling Silver</strong>
+                                           </h6> -->
+                                        <h6 class="text-main font-size-md p-r-10x">
+                                            <strong>{{$showSku['main_title']}}</strong></h6>
+                                        <span class="text-primary font-size-sm flex-fixedShrink">${{$showSku['sale_price']}}</span>
+                                    </article>
+                                    <aside class="cartItem-secondaryInfo text-primary font-size-sm">
+                                        <div><span>Size: </span><span>11</span></div>
+                                        <div><span>Color: </span><span>Black</span></div>
+                                        <div><span>Material: </span><span>Gold</span></div>
+                                        <div class="flex flex-fullJustified">
+                                            <div class="">
+                                                <span>Inside Engraving: </span><span>MY LOVE</span></div>
+                                            <div class="">${{$showSku['total_amount']}}</div>
+                                        </div>
+                                    </aside>
+                                </div>
+                                <div class="mask"></div>
                             </div>
-                            <div class="p-l-10x flex-width">
-                                <article class="flex flex-fullJustified">
-                                 <!--<h6 class="text-main font-size-md p-r-10x">
-                                        <strong>Crown Shape Black Gold-plated Sterling Silver Engagehape Black
-                                                Gold-plated
-                                                Sterling Silver</strong>
-                                    </h6> -->
-                                    <h6 class="text-main font-size-md p-r-10x"><strong>{{$showSku['main_title']}}</strong></h6>
-                                    <span class="text-primary font-size-sm flex-fixedShrink">${{$showSku['sale_price']}}</span>
-                                </article>
-                                <aside class="cartItem-secondaryInfo text-primary font-size-sm">
-                                    <div><span>Size: </span><span>11</span></div>
-                                    <div><span>Color: </span><span>Black</span></div>
-                                    <div><span>Material: </span><span>Gold</span></div>
-                                    <div class="flex flex-fullJustified">
-                                        <div class="">
-                                            <span>Inside Engraving: </span><span>MY LOVE</span></div>
-                                        <div class="">${{$showSku['total_amount']}}</div>
-                                    </div>
-                                </aside>
-                            </div>
-                            <div class="mask"></div>
-                        </div>
-                        <div class="flex flex-alignCenter flex-fullJustified p-y-10x">
-                            <div class="flex">
-                                <a class="btn btn-cartUpdate btn-sm" data-remodal-target="modal" data-sku="{{$showSku['sku']}}" data-action="delsku">Delete</a>
-                                <a href="#" class="btn btn-cartUpdate btn-sm" data-product-move="save" data-sku="{{$showSku['sku']}}">Save for Later</a>
-                            </div>
-                            <div class="flex flex-alignCenter">
-                                <span class="text-primary font-size-sm m-r-5x">Qty:</span>
-                                <div class="btn-group flex" data-sku="{{$showSku['sku']}}">
-                                    <div class="btn btn-cartCount btn-sm @if(1 !== $showSku['stock_status']) disabled @endif" data-item-qty="minus">
-                                        <i class="iconfont icon-minus"></i>
-                                    </div>
-                                    <div class="btn btn-cartCount btn-sm" data-item-num="{{$showSku['sale_qtty']}}">{{$showSku['sale_qtty']}}</div>
-                                    <div class="btn btn-cartCount btn-sm @if(1 !== $showSku['stock_status']) disabled @endif" data-item-qty="add">
-                                        <i class="iconfont icon-add"></i>
+                            <div class="flex flex-alignCenter flex-fullJustified p-y-10x">
+                                <div class="flex">
+                                    <a class="btn btn-cartUpdate btn-sm" data-remodal-target="modal"
+                                       data-sku="{{$showSku['sku']}}" data-action="delsku">Delete</a>
+                                    <a href="#" class="btn btn-cartUpdate btn-sm" data-product-move="save"
+                                       data-sku="{{$showSku['sku']}}">Save for Later</a>
+                                </div>
+                                <div class="flex flex-alignCenter">
+                                    <span class="text-primary font-size-sm m-r-5x">Qty:</span>
+                                    <div class="btn-group flex" data-sku="{{$showSku['sku']}}">
+                                        <div class="btn btn-cartCount btn-sm @if(1 !== $showSku['stock_status']) disabled @endif"
+                                             data-item-qty="minus">
+                                            <i class="iconfont icon-minus"></i>
+                                        </div>
+                                        <div class="btn btn-cartCount btn-sm"
+                                             data-item-num="{{$showSku['sale_qtty']}}">{{$showSku['sale_qtty']}}</div>
+                                        <div class="btn btn-cartCount btn-sm @if(1 !== $showSku['stock_status']) disabled @endif"
+                                             data-item-qty="add">
+                                            <i class="iconfont icon-add"></i>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="text-warming font-size-xs">Warming: Women’s Ring</div>
                         </div>
-                        <div class="text-warming font-size-xs">Warming: Women’s Ring</div>
-                    </div>
-					@endforeach
+                    @endforeach
                 </section>
 
                 <!-- 商品总价 -->
                 <section class="bg-white m-t-10x p-a-10x">
                     <div class="flex flex-rightJustify text-primary font-size-sm">
-                        <span class="p-r-5x">Items({{$cartData['total_sku_qtty'] }}): </span><strong>${{$cartData['total_amount']}}</strong>
+                        <span class="p-r-5x">Items({{$cartData['total_sku_qtty'] }}
+                            ): </span><strong>${{$cartData['total_amount']}}</strong>
                     </div>
                     <div class="flex flex-rightJustify text-primary font-size-sm">
                         <span class="p-r-5x">Extra: </span><strong>${{ $cartData['vas_amount'] }}</strong>
@@ -109,74 +118,78 @@
                     <a href="#" class="btn btn-primary btn-block btn-sm" type="submit">Proceed To Checkout</a>
                 </section>
 
-				@endif
+        @endif
 
-				@if(!empty($saveData['showSkus']))
-                <!-- 暂存商品列表 -->
+        @if(!empty($saveData['showSkus']))
+            <!-- 暂存商品列表 -->
                 <article class="font-size-md text-main p-a-10x"><strong>Saved Items</strong></article>
                 <!-- 商品列表 -->
                 <section class="cartList bg-white">
-					@foreach($saveData['showSkus'] as $showSku)
-                    <div class="cartList-item p-a-10x @if( 1 !== $showSku['stock_status'] ) disabled @endif">
-                        <div class="productInfo flex">
-                            <div class="flex-fixedShrink">
-                                <img class="img-thumbnail" src="{{ 'https://s3-us-west-1.amazonaws.com/emimagetest/n2/'.$showSku['main_image_url'] }}" width="70px" height="70px">
+                    @foreach($saveData['showSkus'] as $showSku)
+                        <div class="cartList-item p-a-10x @if( 1 !== $showSku['stock_status'] ) disabled @endif">
+                            <div class="productInfo flex">
+                                <div class="flex-fixedShrink">
+                                    <img class="img-thumbnail"
+                                         src="{{ 'https://s3-us-west-1.amazonaws.com/emimagetest/n2/'.$showSku['main_image_url'] }}"
+                                         width="70px" height="70px">
+                                </div>
+                                <div class="p-l-10x flex-width">
+                                    <article class="flex flex-fullJustified">
+                                        <h6 class="text-main font-size-md p-r-10x">
+                                            <!--<strong>Crown Shape Black Gold-plated Sterling Silver Engage...</strong>-->
+                                            <strong>{{$showSku['main_title']}}</strong>
+                                        </h6>
+                                        <span class="text-primary font-size-sm flex-fixedShrink">${{$showSku['sale_price']}}</span>
+                                    </article>
+                                    <aside class="cartItem-secondaryInfo text-primary font-size-sm">
+                                        <div><span>Size: </span><span>11</span></div>
+                                        <div><span>Color: </span><span>Black</span></div>
+                                        <div><span>Material: </span><span>Gold</span></div>
+                                        <div class="flex flex-fullJustified">
+                                            <div class="">
+                                                <span>Inside Engraving: </span><span>MY LOVE</span></div>
+                                            <div class="">${{$showSku['total_amount']}}</div>
+                                        </div>
+                                    </aside>
+                                </div>
+                                <div class="mask"></div>
                             </div>
-                            <div class="p-l-10x flex-width">
-                                <article class="flex flex-fullJustified">
-                                    <h6 class="text-main font-size-md p-r-10x">
-                                        <!--<strong>Crown Shape Black Gold-plated Sterling Silver Engage...</strong>-->
-										<strong>{{$showSku['main_title']}}</strong>
-                                    </h6>
-                                    <span class="text-primary font-size-sm flex-fixedShrink">${{$showSku['sale_price']}}</span>
-                                </article>
-                                <aside class="cartItem-secondaryInfo text-primary font-size-sm">
-                                    <div><span>Size: </span><span>11</span></div>
-                                    <div><span>Color: </span><span>Black</span></div>
-                                    <div><span>Material: </span><span>Gold</span></div>
-                                    <div class="flex flex-fullJustified">
-                                        <div class="">
-                                            <span>Inside Engraving: </span><span>MY LOVE</span></div>
-                                        <div class="">${{$showSku['total_amount']}}</div>
-                                    </div>
-                                </aside>
+                            <div class="flex p-y-10x">
+                                <a class="btn btn-cartUpdate btn-sm" data-remodal-target="modal"
+                                   data-sku="{{$showSku['sku']}}" data-action="delsave">Delete</a>
+                                <a class="btn btn-cartUpdate btn-sm" data-product-move="movetocart"
+                                   data-sku="{{$showSku['sku']}}">Move to Bag</a>
                             </div>
-                            <div class="mask"></div>
                         </div>
-                        <div class="flex p-y-10x">
-                            <a class="btn btn-cartUpdate btn-sm" data-remodal-target="modal" data-sku="{{$showSku['sku']}}" data-action="delsave">Delete</a>
-                            <a class="btn btn-cartUpdate btn-sm" data-product-move="movetocart" data-sku="{{$showSku['sku']}}">Move to Bag</a>
-                        </div>
-                    </div>
-					@endforeach
+                    @endforeach
                 </section>
-			@endif
-            </section>
+            @endif
+        </section>
 
-<!-- 页脚 功能链接 start-->
-@include('footer')
-<!-- 页脚 功能链接 end-->
-        </div>
+        <!-- 页脚 功能链接 start-->
+    @include('footer')
+    <!-- 页脚 功能链接 end-->
     </div>
+</div>
 
-    <!-- 删除将要购买的商品 -->
-    <!-- TODO remodal 有多余的样式 需要整理 -->
-    <div class="remodal remodal-md modal-content" data-remodal-id="modal" id="cartDialog">
-        <div class="font-size-sm p-t-20x p-x-15x p-b-15x">
-            Are you sure you want to remove <br> this item from your bag?
-        </div>
-        <div class="btn-group flex">
-            <div class="btn remodal-btn flex-width" data-remodal-action="confirm">Remove</div>
-            <div class="btn remodal-btn flex-width" data-remodal-action="cancel">Cancel</div>
-        </div>
+<!-- 删除将要购买的商品 -->
+<!-- TODO remodal 有多余的样式 需要整理 -->
+<div class="remodal remodal-md modal-content" data-remodal-id="modal" id="cartDialog">
+    <div class="font-size-sm p-t-20x p-x-15x p-b-15x">
+        Are you sure you want to remove <br> this item from your bag?
     </div>
-    <!-- loading 效果 -->
-    <div class="loading loading-screen loading-switch loading-hidden">
-        <div class="loader loader-screen"></div>
+    <div class="btn-group flex">
+        <div class="btn remodal-btn flex-width" data-remodal-action="confirm">Remove</div>
+        <div class="btn remodal-btn flex-width" data-remodal-action="cancel">Cancel</div>
     </div>
+</div>
+<!-- loading 效果 -->
+<div class="loading loading-screen loading-switch loading-hidden">
+    <div class="loader loader-screen"></div>
+</div>
 
 </body>
 <script src="/scripts/vendor.js"></script>
 
-<script src="/scripts/shoppingList.js"></script>
+<script src="/scripts/shoppingCart.js"></script>
 </html>
