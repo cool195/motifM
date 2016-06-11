5a4fad2c19cf0fa6cc5b2fabd089ccdca3137722
zhangtao <250574087@qq.com>
2016-06-11 12:57:00 +0800

    登录页面添加csrf_token验证

--- resources/views/shopping/login.blade.php
+++ resources/views/shopping/login.blade.php
@@ -78,4 +78,12 @@
 <script src="/scripts/vendor.js"></script>
 
 <script src="/scripts/login.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 </html>
