610d72f3c56af91bd1f13624bf8a889a2fbef38c
zhaozhe <zhaozhe@evermarker.net>
2016-06-11 18:39:57 +0800

    update 更新 个人中心 地址列表选择 profileSetting-addressList 页面

--- resources/static/dist/scripts/profileSetting-addressList.js
+++ resources/static/dist/scripts/profileSetting-addressList.js
@@ -1,16 +1,69 @@
-/**
- * Created by zhaozhe on 16/5/23.
- */
-/*global jQuery*/
-
 'use strict';
 
 (function ($) {
-    var options = {
-        closeOnOutsideClick: false,
-        hashTracking: true
-    };
+    /**
+     *
+     * @param $Edit
+     */
+    function switchEdit($Edit) {
+        if ($Edit.hasClass('active')) {
+            $Edit.html('Edit');
+            $Edit.toggleClass('active');
+            $Edit.addClass('btn-primary-outline').removeClass('btn-primary');
+            $('div[data-role="submit"]').removeClass('hidden');
+        } else {
+            $Edit.html('Done');
+            $Edit.toggleClass('active');
+            $Edit.addClass('btn-primary').removeClass('btn-primary-outline');
+            $('div[data-role="submit"]').addClass('hidden');
+        }
+    }
 
+    /**
+     *
+     * @param $Edit
+     */
+    function switchSelect($Edit) {
+        var $IconFont = $('.addressItem-info').find('.iconfont');
+        if ($Edit.hasClass('active')) {
+            $IconFont.removeClass('icon-arrow-right').addClass('icon-radio');
+        } else {
+            $IconFont.removeClass('icon-radio').addClass('icon-arrow-right');
+        }
+    }
+
+    /**
+     *
+     * @param $Edit
+     */
+    function switchLink($Edit) {
+        var $LinkList = $('.addressItem-info');
+
+        if ($Edit.hasClass('active')) {
+            $.each($LinkList, function (index, val) {
+                var Action = $(val).data('url-return');
+                $(val).attr('data-action', Action);
+            });
+        } else {
+            $.each($LinkList, function (index, val) {
+                var Action = $(val).data('url-edit');
+                $(val).attr('data-action', Action);
+            });
+        }
+    }
+
+    $('#address-edit').on('click', function (e) {
+        // 可选的状态切换
+        switchSelect($(e.target));
+
+        // 跳转链接的切换
+        switchLink($(e.target));
+
+        // 切换按钮以及叉号状态
+        switchEdit($(e.target));
+
+        $('.addressList-delete').toggleClass('switch');
+    });
     // loading 打开
     function openLoading() {
         $('.loading').toggleClass('loading-hidden');
@@ -27,58 +80,69 @@
         }, 500);
     }
 
-    // 触发编辑地址事件 更改按钮样式
-    $('#btnEdit').on('click', function () {
-        // 修改状态
-        if ($(this).hasClass('btn-edit')) {
-            $(this).removeClass('btn-edit').addClass('btn-done').html('Done');
-            $('#addressList').addClass('editing');
-        } else {
-            //修改完成状态
-            $(this).removeClass('btn-done').addClass('btn-edit').html('Edit');
-            $('#addressList').removeClass('editing');
-        }
-    });
-
-    // 删除送货地址
-    function deladdress(aid) {
+    /**
+     *
+     * @param AddressId
+     */
+    function deleteAddress(AddressID) {
         openLoading();
+
         $.ajax({
-            url: '/useraddr/delUserAddress',
+            url: '/addresses',
             type: 'DELETE',
-            data: { cmd: 'del', aid: aid }
-        }).done(function () {
-            console.log('success');
-            closeLoading();
+            data: { aid: AddressID }
+        }).done(function (data) {
+            // TODO 请求成功后 删除相应地址
+            if (data.success) {
+                console.log("success");
+            }
         }).fail(function () {
-            console.log('error');
+            console.log("error");
         }).always(function () {
+            console.log("complete");
             closeLoading();
-            console.log('complete');
         });
     }
 
-    // 初始化 模态框
-    $('#addressDialog').remodal(options);
-
-    // 触发 删除点
-    $('[data-remodal-target="modal"]').click(function (e) {
-        // 暂存数据到模态框
-        $('#addressDialog').data('aid', $(e.target).data('aid'));
-        console.log('open');
+    // 删除按钮
+    $('.addressList-delete').on('click', function (e) {
+        var AddressID = $(e.target).parents('.addressList-container').data('aid');
+        $('#modalDialog').data('aid', AddressID);
     });
 
-    // 关闭模态框后 去除模态框中的数据
-    $('#addressDialog').on('closed', function () {
-        $(this).removeData('aid');
+    $('.addressItem-info').on('click', function () {
+        var Action = $(this).data('action');
+
+        if (Action === 'return') {
+            $('.icon-radio.active').removeClass('active');
+            $(this).find('.icon-radio').addClass('active');
+        } else if (Action === 'edit') {
+            // TODO 跳转到编辑页面
+        }
+    });
+
+    $('div[data-role="submit"]').on('click', function () {
+        // TODO 提交相应数据到后台
+    });
+
+    // 初始化 模态框
+    $('#modalDialog').remodal({
+        closeOnOutsideClick: false,
+        hashTracking: true
+    });
+
+    $('#modalDialog').on('closed', function () {
+        $(this).removeData('address');
         console.log('close');
     });
 
-    // 模态框 确认删除
-    $('#addressDialog').on('confirmation', function () {
-        var aid = $(this).data('aid');
-        console.info(aid);
-        deladdress(aid);
+    $('#modalDialog').on('confirmation', function () {
+        var AddressID = $(this).data('aid');
+        if (AddressID === undefined || AddressID === null || AddressID === '') {
+            console.log('AddressID 没有值');
+            return;
+        }
+        deleteAddress(AddressID);
     });
 })(jQuery);
 //# sourceMappingURL=profileSetting-addressList.js.map
