92f1916266757ed5c32c2d46ad3ab2ef5b3eedeb
zhaozhe <zhaozhe@evermarker.net>
2016-06-12 16:24:47 +0800

    update 更新模板

--- resources/views/navigator.blade.php
+++ resources/views/navigator.blade.php
@@ -1,20 +1,23 @@
 <header class="navbar-fixed-top" id="header">
-<nav class="navbar navbar-full bg-primary">
-	<ul class="nav navbar-primary">
-		<li class="nav-item">
-			<div class="nav-icon" id="nav-menu-control">
-				<i class="nav-tap iconfont icon-hamburger icon-size-lg"></i>
-			</div>
-		</li>
-		<li class="nav-item nav-logo">
-			<a><img src="/images/logo/logo.png" srcset="/images/logo/logo@2x.png 2x,/images/logo/logo@3x.png 3x"></a>
-		</li>
-		<li class="nav-item">
-			<div class="nav-shoppingCart">
-				<a href="/shopping/cart"><i class="nav-tap iconfont icon-shopbag icon-size-lg"></i></a>
-				<span class="shoppingCart-number">2</span>
-			</div>
-		</li>
-	</ul>
-</nav>
+    <nav class="navbar navbar-full bg-primary">
+        <ul class="nav navbar-primary">
+            <li class="nav-item">
+                <div class="nav-icon" id="nav-menu-control">
+                    <i class="nav-tap iconfont icon-hamburger icon-size-lg"></i>
+                </div>
+            </li>
+            <li class="nav-item nav-logo">
+                <a><img src="/images/logo/logo.png"
+                        srcset="/images/logo/logo@2x.png 2x,/images/logo/logo@3x.png 3x"></a>
+            </li>
+            <li class="nav-item">
+                <a href="/shopping/cart">
+                    <div class="nav-shoppingCart">
+                        <i class="nav-tap iconfont icon-shopbag icon-size-lg"></i>
+                        <span class="shoppingCart-number">2</span>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </nav>
 </header>
