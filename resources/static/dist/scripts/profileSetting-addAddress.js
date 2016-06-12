7341be40e2f063f558daf57595fc75cfb148a94e
zhaozhe <zhaozhe@evermarker.net>
2016-06-12 10:38:16 +0800

    update 更新静态页面

--- resources/static/dist/scripts/profileSetting-addAddress.js
+++ resources/static/dist/scripts/profileSetting-addAddress.js
@@ -7,11 +7,14 @@
 
 (function ($) {
     // 设置默认地址 开关按钮
-    $('#bg-openClose').on('click', function () {
-        var classname = $(this).attr('class') === 'close' ? 'open' : 'close';
-        var btnclassname = $('#btn-openClose').attr('class') === 'btn-close' ? 'btn-open' : 'btn-close';
-        $(this).attr('class', classname);
-        $('#btn-openClose').attr('class', btnclassname);
+    $('.radio-checkBox').on('click', function () {
+        $(this).toggleClass('open');
+
+        if ($(this).hasClass('open')) {
+            $('#address-primary').prop('checked', true);
+        } else {
+            $('#address-default').prop('checked', true);
+        }
     });
 
     // loading 打开
@@ -34,38 +37,12 @@
     function addUserAddress() {
         openLoading();
         // 获取表单数据
-        var name = $('#name').val();
-        var addr1 = $('#addr1').val();
-        var addr2 = $('#addr2').val();
-        var state = $('#state').val();
-        var city = $('#city').val();
-        var zip = $('#zip').val();
-        var tel = $('#tel').val();
-        var idnum = $('#idnum').val();
-        var country = $('#country').data('country');
-        var isd = 0;
-        if ($('#bg-openClose').hasClass('open')) {
-            isd = 1;
-        }
         $.ajax({
             url: '/useraddr/addUserAddress',
             type: 'POST',
-            data: {
-                cmd: 'add',
-                name: name,
-                addr1: addr1,
-                addr2: addr2,
-                state: state,
-                city: city,
-                zip: zip,
-                tel: tel,
-                idnum: idnum,
-                country: country,
-                isd: isd
-            }
+            data: $('#addressInfo').serialize()
         }).done(function () {
             console.log('success');
-            window.history.back(-1);
         }).fail(function () {
             console.log('error');
         }).always(function () {
@@ -74,14 +51,33 @@
         });
     }
 
+    // 表单非空验证
+    function checkInput() {
+        var Result = true;
+        $('input[type="text"]').each(function () {
+            if ($(this).val() === '') {
+                Result = false;
+                return Result;
+            }
+            // TODO 添加警告
+        });
+        return Result;
+    }
+
+    // 表单格式验证
+
     // 点击提交表单
-    $('#btn-addAddress').click(function () {
-        addUserAddress();
+    $('#btn-addAddress').on('click', function (e) {
+        $(e.target).removeClass('disabled');
+        // 表单非空验证
+        if (checkInput()) {
+            addUserAddress();
+        } else {
+            $(e.target).addClass('disabled');
+        }
     });
 
     // 退出添加
-    $('#Cancel').click(function () {
-        window.history.back(-1);
-    });
+    $('#Cancel').on('click', function () {});
 })(jQuery);
 //# sourceMappingURL=profileSetting-addAddress.js.map
