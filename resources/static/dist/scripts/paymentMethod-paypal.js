622721bc22f57d297947b5c6d02b68cc8cabea6b
zhaozhe <zhaozhe@evermarker.net>
2016-06-14 13:08:53 +0800

    更新静态文件

--- resources/static/dist/scripts/paymentMethod-paypal.js
+++ resources/static/dist/scripts/paymentMethod-paypal.js
@@ -1,44 +1,68 @@
-"use strict";
+'use strict';
 
-// 客户端验证 token
-var token = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJjZTRjNzVjZTkwMjRiNGVkMTYxMzlmNTlmODNiYmVmMjRmNDFjMjZjM2U3YjMyZmE0YzI5YzdmMjJiNDAyOTQ0fGNyZWF0ZWRfYXQ9MjAxNi0wNi0xM1QxMDo1NDozMy4wMzcxMDk3ODArMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tLzM0OHBrOWNnZjNiZ3l3MmIifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJjb2luYmFzZUVuYWJsZWQiOmZhbHNlLCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0=";
-
-braintree.setup(token, "custom", {
-    paypal: {
-        singleUse: true, // 沙盒系统需要字段
-        amount: 10.00, // 沙盒系统需要字段
-        currency: 'USD', // 沙盒系统需要字段
-        locale: 'en_us', // 沙盒系统需要字段
-        headless: true
-    },
-    onReady: function onReady(integration) {
-        checkout = integration;
-    },
-    onPaymentMethodReceived: function onPaymentMethodReceived(payload) {
-        console.info('payload : ');
-        console.info(payload);
-
-        openLoading();
-        // TODO
-        $.ajax({
-            url: '/braintree',
-            type: 'POST',
-            data: { nonc: payload.nonc }
-        }).done(function (data) {
-            if (data.success) {
-                console.log("success");
-            }
-        }).fail(function () {
-            console.log("error");
-        }).always(function () {
-            closeLoading();
-            console.log("complete");
-        });
+(function () {
+    // loading 打开
+    function openLoading() {
+        $('.loading').toggleClass('loading-hidden');
+        setTimeout(function () {
+            $('.loading').toggleClass('loading-open');
+        }, 25);
     }
-});
 
-$('#paypal').on('click', function (event) {
-    event.preventDefault();
-    checkout.paypal.initAuthFlow();
-});
+    // loading 隐藏
+    function closeLoading() {
+        $('.loading').addClass('loading-close');
+        setTimeout(function () {
+            $('.loading').toggleClass('loading-hidden loading-open').removeClass('loading-close');
+        }, 500);
+    }
+
+    // 客户端验证 token
+    // var token = $('#paypal').data('braintree');
+    // 测试 token
+    var token = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJjZTRjNzVjZTkwMjRiNGVkMTYxMzlmNTlmODNiYmVmMjRmNDFjMjZjM2U3YjMyZmE0YzI5YzdmMjJiNDAyOTQ0fGNyZWF0ZWRfYXQ9MjAxNi0wNi0xM1QxMDo1NDozMy4wMzcxMDk3ODArMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tLzM0OHBrOWNnZjNiZ3l3MmIifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJjb2luYmFzZUVuYWJsZWQiOmZhbHNlLCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0=";
+    // braintree 初始化
+
+    var checkout = {}; // 事件 句柄
+
+    braintree.setup(token, "custom", {
+        paypal: {
+            singleUse: true, // 沙盒系统需要字段
+            amount: 10.00, // 沙盒系统需要字段
+            currency: 'USD', // 沙盒系统需要字段
+            locale: 'en_us', // 沙盒系统需要字段
+            headless: true
+        },
+        onReady: function onReady(integration) {
+            checkout = integration;
+        },
+        onPaymentMethodReceived: function onPaymentMethodReceived(payload) {
+            console.info('payload : ');
+            console.info(payload);
+
+            openLoading();
+            // TODO
+            $.ajax({
+                url: '/braintree',
+                type: 'POST',
+                data: { nonc: payload.nonc }
+            }).done(function (data) {
+                if (data.success) {
+                    console.log("success");
+                    location.reload();
+                }
+            }).fail(function () {
+                console.log("error");
+            }).always(function () {
+                closeLoading();
+                console.log("complete");
+            });
+        }
+    });
+
+    $('#paypal').on('click', function (event) {
+        event.preventDefault();
+        checkout.paypal.initAuthFlow();
+    });
+})();
 //# sourceMappingURL=paymentMethod-paypal.js.map
