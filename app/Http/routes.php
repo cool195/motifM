8668476edae7ccd5dd9d965df8a6662a9d795e29
zhangtao <250574087@qq.com>
2016-06-14 10:56:56 +0800

    路由添加拦截器

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,104 +25,103 @@ $app->get('/shopping', 'Shopping\ShoppingController@index');
 //$app->get('/stock/checkstock', 'Shopping\ShoppingController@checkStock');
 
 
-$app->group([/*'middleware'=>'logincheck',*/'namespace'=>'App\Http\Controllers\Shopping'], function($app){
+$app->group(['middleware'=>'logincheck', 'namespace'=>'App\Http\Controllers\Shopping'], function($app){
+
+    $app->get('/products/{spu}', 'ProductController@getProductDetail');
+    $app->get('/detail/{spu}', 'ProductController@index');
+
+
+    $app->get('/shopping/cart', 'CartController@index');
+    $app->get('/shopping/ordercheckout', 'CartController@orderCheckout');
+    $app->get('/shopping/cart/addresslist', 'CartController@addressList');
+    $app->get('/shopping/cart/coupon', 'CartController@coupon');
+    $app->get('/shopping/cart/message', 'CartController@message');
+
+
+    $app->get('/cart/amount', 'CartController@getCartAmount');
+    $app->get('/cart/list', 'CartController@getCartList');
+    $app->get('/cart/accountlist', 'CartController@getCartAccountList');
+    $app->get('/cart/savelist', 'CartController@getCartSaveList');
+//$app->get('/shopping/cart/addCart', 'Shopping\CartController@addCart');
+    $app->patch('/cart', 'CartController@addCart');
+    $app->put('/cart', 'CartController@promptlyBuy');
+    $app->get('/shopping/cart/addBatchCart', 'CartController@addBatchCart');
+    $app->get('/cart/alterQtty', 'CartController@alterCartProQtty');
+
+//$app->get('/shopping/cart/promptlyBuy', 'Shopping\CartController@promptlyBuy');
+    $app->get('/cart/operate', 'CartController@operateCartProduct');
+    $app->post('/cart/operate', 'CartController@operateCartProduct');
+
+    $app->get('/pay/paymentmethod', 'PayController@paymentMethod');
+    $app->get('/pay/cardadd', 'PayController@newCardAdd');
+
+    $app->get('/pay/token', 'PayController@getPayToken');
+    $app->get('/shopping/pay/method', 'PayController@createPayMethod');
+    $app->get('/shopping/pay/pay', 'PayController@pay');
+    $app->get('/shopping/pay/checkpay', 'PayController@pay');
+    $app->get('/pay/methodlist', 'PayController@getMethodList');
+    $app->get('/shopping/pay/del', 'PayController@delMethod');
+    $app->get('/shopping/pay/getDefault', 'PayController@getDefaultMethod');
+    $app->get('/shopping/pay/setDefault', 'PayController@setDefaultMethod');
+    $app->get('/shopping/pay/check', 'PayController@check');
+
+    $app->get('/addr/list', 'AddressController@getUserAddrList');
+    $app->get('/addr/default', 'Shopping\AddressController@getUserDefaultAddr');
+    $app->get('/addr/add', 'AddressController@addUserAddr');
+    $app->post('/addr/add', 'AddressController@addUserAddr');
+    $app->get('/addr/modify', 'AddressController@modifyUserAddr');
+    $app->post('/addr/modify', 'AddressController@modifyUserAddr');
+    $app->get('/addr/mdefault', 'AddressController@modifyUserDefaultAddr');
+    $app->get('/addr/del', 'AddressController@delUserAddr');
+    $app->delete('/addresses', 'AddressController@delUserAddr');
+    $app->get('/addr/country', 'AddressController@getCountry');
+
+
+
+
+    $app->get('/reset', 'UserController@reset');
+    $app->get('/user/setting', 'UserController@setting');
+    $app->get('/user/changeprofile', 'UserController@changeProfile');
+    $app->get('/user/shippingaddress', 'UserController@shippingAddress');
+    $app->get('/user/addradd', 'UserController@addrAdd');
+    $app->get('/user/addrmod/{aid}', 'UserController@addrModify');
+    $app->get('/user/countrylist', 'UserController@countryList');
+    $app->get('/user/changepassword', 'UserController@changePassword');
+
+
+    $app->get('/user/signout', 'UserController@signout');
+    $app->get('/user/resetPwd', 'UserController@resetPassword');
+    $app->get('/user/forget', 'UserController@forgetPassword');
+    $app->post('/user/modifyUserPwd', 'UserController@modifyUserPwd');
+    $app->get('/user/trilogin', 'UserController@tryPrtLogin');
+    $app->get('/user/userdetail', 'UserController@getUserDetailInfo');
+    $app->get('/user/modifyUserInfo', 'UserController@modifyUserInfo');
+    $app->post('/user/modifyUserInfo', 'UserController@modifyUserInfo');
+
+
+
+    $app->get('/shopping/orderlist', 'OrderController@index');
+//$app->get('/shopping/order/orderlist', 'Shopping\OrderController@getOrderList');
+    $app->get('/orders', 'OrderController@getOrderList');
+    $app->get('/shopping/order/orderdetail/{subno}', 'OrderController@orderDetail');
+    $app->get('/shopping/order/orderSubmit', 'OrderController@orderSubmit');
+});
+
+$app->group(['namespace'=>'App\Http\Controllers\Shopping'], function($app){
     $app->get('/shopping', 'ShoppingController@index');
     $app->get('/category', 'ShoppingController@getShoppingCategoryList');
     $app->get('/products', 'ShoppingController@getShoppingProductList');
     $app->get('/stock/checkstock', 'ShoppingController@checkStock');
-
-
-    $app->get('/products/{spu}', 'ProductController@getProductDetail');
-    $app->get('/detail/{spu}', 'ProductController@index');
 });
-
-$app->get('/shopping/cart', 'Shopping\CartController@index');
-$app->get('/shopping/ordercheckout', 'Shopping\CartController@orderCheckout');
-$app->get('/shopping/cart/addresslist', 'Shopping\CartController@addressList');
-$app->get('/shopping/cart/coupon', 'Shopping\CartController@coupon');
-$app->get('/shopping/cart/message', 'Shopping\CartController@message');
-
-
-
-$app->get('/cart/amount', 'Shopping\CartController@getCartAmount');
-$app->get('/cart/list', 'Shopping\CartController@getCartList');
-$app->get('/cart/accountlist', 'Shopping\CartController@getCartAccountList');
-$app->get('/cart/savelist', 'Shopping\CartController@getCartSaveList');
-//$app->get('/shopping/cart/addCart', 'Shopping\CartController@addCart');
-$app->patch('/cart', 'Shopping\CartController@addCart');
-$app->put('/cart', 'Shopping\CartController@promptlyBuy');
-$app->get('/shopping/cart/addBatchCart', 'Shopping\CartController@addBatchCart');
-$app->get('/cart/alterQtty', 'Shopping\CartController@alterCartProQtty');
-
-//$app->get('/shopping/cart/promptlyBuy', 'Shopping\CartController@promptlyBuy');
-$app->get('/cart/operate', 'Shopping\CartController@operateCartProduct');
-$app->post('/cart/operate', 'Shopping\CartController@operateCartProduct');
-
-
-$app->get('/pay/paymentmethod', 'Shopping\PayController@paymentMethod');
-$app->get('/pay/cardadd', 'Shopping\PayController@newCardAdd');
-
-$app->get('/pay/token', 'Shopping\PayController@getPayToken');
-$app->get('/shopping/pay/method', 'Shopping\PayController@createPayMethod');
-$app->get('/shopping/pay/pay', 'Shopping\PayController@pay');
-$app->get('/shopping/pay/checkpay', 'Shopping\PayController@pay');
-$app->get('/pay/methodlist', 'Shopping\PayController@getMethodList');
-$app->get('/shopping/pay/del', 'Shopping\PayController@delMethod');
-$app->get('/shopping/pay/getDefault', 'Shopping\PayController@getDefaultMethod');
-$app->get('/shopping/pay/setDefault', 'Shopping\PayController@setDefaultMethod');
-$app->get('/shopping/pay/check', 'Shopping\PayController@check');
-
-
-
-$app->get('/addr/list', 'Shopping\AddressController@getUserAddrList');
-$app->get('/addr/default', 'Shopping\AddressController@getUserDefaultAddr');
-$app->get('/addr/add', 'Shopping\AddressController@addUserAddr');
-$app->post('/addr/add', 'Shopping\AddressController@addUserAddr');
-$app->get('/addr/modify', 'Shopping\AddressController@modifyUserAddr');
-$app->post('/addr/modify', 'Shopping\AddressController@modifyUserAddr');
-$app->get('/addr/mdefault', 'Shopping\AddressController@modifyUserDefaultAddr');
-$app->get('/addr/del', 'Shopping\AddressController@delUserAddr');
-$app->delete('/addresses', 'Shopping\AddressController@delUserAddr');
-$app->get('/addr/country', 'Shopping\AddressController@getCountry');
 
 
 
 $app->get('/login', 'Shopping\UserController@login');
-$app->get('/register', 'Shopping\UserController@register');
-$app->get('/reset', 'Shopping\UserController@reset');
-$app->get('/user/setting', 'Shopping\UserController@setting');
-$app->get('/user/changeprofile', 'Shopping\UserController@changeProfile');
-$app->get('/user/shippingaddress', 'Shopping\UserController@shippingAddress');
-$app->get('/user/addradd', 'Shopping\UserController@addrAdd');
-$app->get('/user/addrmod/{aid}', 'Shopping\UserController@addrModify');
-$app->get('/user/countrylist', 'Shopping\UserController@countryList');
-$app->get('/user/changepassword', 'Shopping\UserController@changePassword');
-
-
-$app->get('/user/signup', 'Shopping\UserController@signup');
-$app->post('/user/signup', 'Shopping\UserController@signup');
 $app->get('/user/logincheck', 'Shopping\UserController@loginCheck');
 $app->post('/user/logincheck', 'Shopping\UserController@loginCheck');
-$app->get('/user/signout', 'Shopping\UserController@signout');
-$app->get('/user/resetPwd', 'Shopping\UserController@resetPassword');
-$app->get('/user/forget', 'Shopping\UserController@forgetPassword');
-$app->post('/user/modifyUserPwd', 'Shopping\UserController@modifyUserPwd');
-$app->get('/user/trilogin', 'Shopping\UserController@tryPrtLogin');
-$app->get('/user/userdetail', 'Shopping\UserController@getUserDetailInfo');
-$app->get('/user/modifyUserInfo', 'Shopping\UserController@modifyUserInfo');
-$app->post('/user/modifyUserInfo', 'Shopping\UserController@modifyUserInfo');
-
-
-
-$app->get('/shopping/orderlist', 'Shopping\OrderController@index');
-//$app->get('/shopping/order/orderlist', 'Shopping\OrderController@getOrderList');
-$app->get('/orders', 'Shopping\OrderController@getOrderList');
-$app->get('/shopping/order/orderdetail/{subno}', 'Shopping\OrderController@orderDetail');
-$app->get('/shopping/order/orderSubmit', 'Shopping\OrderController@orderSubmit');
-
-//$app->get('/category', 'Shopping\ShoppingController@getShoppingCategoryList');
-//$app->get('/products', 'Shopping\ShoppingController@getShoppingProductList');
-//$app->get('/products/{spu}', 'Shopping\ProductController@getProductDetail');
+$app->get('/register', 'Shopping\UserController@register');
+$app->get('/user/signup', 'UserController@signup');
+$app->post('/user/signup', 'UserController@signup');
 
 $app->get('/designer', 'Designer\DesignerController@index');
 $app->get('/designer/{id}', 'Designer\DesignerController@show');
