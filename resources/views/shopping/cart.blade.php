1464ad08f694338a731969207c57a5cbbdb64307
SuperJu <juchao1989@foxmail.com>
2016-06-17 19:00:56 +0800

    修改cart按钮宽度

--- resources/views/shopping/cart.blade.php
+++ resources/views/shopping/cart.blade.php
@@ -34,7 +34,7 @@
                     <div class="container shopbag-emptyInfo">
                         <div class="m-b-20x p-b-5x"><i class="btn-shopbagEmpty iconfont icon-shopbag"></i></div>
                         <p class="text-primary font-size-sm m-b-20x p-b-20x">No saved items</p>
-                        <a href="/shopping" class="btn btn-primary btn-block btn-sm">Continue Shopping</a>
+                        <a href="/shopping" class="btn btn-primary btn-sm">Continue Shopping</a>
                     </div>
                 </div>
         @else
