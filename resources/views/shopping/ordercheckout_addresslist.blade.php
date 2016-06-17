38030023fbe1624a7aa190d0953a1f4be321af3f
zhangtao <250574087@qq.com>
2016-06-17 11:21:43 +0800

    bug fixed

--- resources/views/shopping/ordercheckout_addresslist.blade.php
+++ resources/views/shopping/ordercheckout_addresslist.blade.php
@@ -107,4 +107,12 @@
 <script src="/scripts/vendor.js"></script>
 
 <script src="/scripts/orderCheckout-addressList.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 </html>
