6c4a633f04740173fe2672b4f36063225113e437
zhangtao <250574087@qq.com>
2016-06-22 14:02:17 +0800

    解决地址修改页面，点击默认地址时，Make Primary没有处于选择状态

--- resources/views/shopping/ordercheckout_modaddress.blade.php
+++ resources/views/shopping/ordercheckout_modaddress.blade.php
@@ -82,7 +82,7 @@
                     <fieldset>
                         <div class="flex flex-alignCenter flex-fullJustified font-size-sm text-primary p-a-15x" href="#">
                             <span>Make Primary</span>
-                            <div class="radio-checkBox">
+                            <div class="radio-checkBox @if(1 == $input['isDefault']) open @endif">
                                 <div class="radio-checkItem"></div>
                                 @if(1 == $input['isDefault'])
                                     <input type="radio" name="isd" id="address-default" hidden value="0">
