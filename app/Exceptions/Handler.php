30be3aa4f13b566452044aadfce208edb43bf41b
SuperJu <juchao1989@foxmail.com>
2016-06-27 18:09:09 +0800

    添加404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,7 +22,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Exception $e
      * @return void
      */
     public function report(Exception $e)
@@ -33,12 +33,44 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
+        if (!env('APP_DEBUG') and $this->isHttpException($e)) {
+            return $this->renderHttpException($e);
+        }
         return parent::render($request, $e);
     }
+
+    /**
+     * Render the given HttpException.
+     *
+     * @param  \Symfony\Component\HttpKernel\Exception\HttpException $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function renderHttpException(HttpException $e)
+    {
+        $status = $e->getStatusCode();
+
+        if (view()->exists("errors.{$status}")) {
+            return response(view("errors.{$status}", []), $status);
+        } else {
+            return (new SymfonyExceptionHandler(env('APP_DEBUG', false)))->createResponse($e);
+        }
+    }
+
+    /**
+     * Determine if the given exception is an HTTP exception.
+     *
+     * @param  \Exception $e
+     * @return bool
+     */
+    protected function isHttpException(Exception $e)
+    {
+        return $e instanceof HttpException;
+    }
+
 }
