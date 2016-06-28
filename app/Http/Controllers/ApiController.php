227d109d5e8b08f6ab76d6f24a1b5ef012671474
SuperJu <juchao1989@foxmail.com>
2016-06-28 14:18:35 +0800

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,32 +13,18 @@ use App\Services\Net;
 
 abstract class ApiController extends Controller
 {
-
-    private $sessionId = "";
     /**
      * 接口地址数组
      *
      * @var array
      */
     protected $ApiUrl = [
-        //接口名称
         'openapi_local' => 'http://192.168.0.230',//本地
         'openapi_test' => 'http://192.168.0.230',//预发布
         'openapi' => 'http://54.222.233.255',//生产
     ];
 
-    public function __construct()
-    {
-        if (Cache::has('frontend')) {
-            $this->sessionId = Cache::get('frontend');
-        } else {
-            $this->sessionId = md5(time());
-            $expiresAt = Carbon::now()->addMinutes(30);
-            Cache::put('frontend', $this->sessionId, $expiresAt);
-        }
-    }
-
-    protected function request($ApiName, $system, $service, array $params, $cacheTime = 0, $output = false)
+    protected function request($ApiName, $system, $service, array $params, $cacheTime = 0)
     {
         $ApiName = $_SERVER['SERVER_NAME'] == 'm.motif.me' ? 'openapi' : ($_SERVER['SERVER_NAME'] == 'motif.app' ? 'openapi_local' : 'openapi_test');
         $buildParams = http_build_query($params);
@@ -48,17 +34,12 @@ abstract class ApiController extends Controller
             $result = Cache::get($key);
         }
         if (empty($result) || "" == $result) {
-            $result = Net::api($this->ApiUrl[$ApiName], $system, $service, $buildParams, ['Cookie:frontend=' . $this->sessionId]);
+            $result = Net::api($this->ApiUrl[$ApiName], $system, $service, $buildParams);
             if ($cacheTime > 0) {
                 Cache::put($key, $result, $cacheTime);
             }
         }
-        if ($output) {
-            return response($result)->header('Content-Type', 'application/json');
-        } else {
-            return json_decode($result, true);
-        }
-
+        return json_decode($result, true);
     }
 
 }
