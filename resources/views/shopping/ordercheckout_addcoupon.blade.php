dc5344e1c723fe6710cb8141d4262cca446e0870
zhangtao <250574087@qq.com>
2016-06-29 16:31:16 +0800

    bug fixed

--- resources/views/shopping/ordercheckout_addcoupon.blade.php
+++ resources/views/shopping/ordercheckout_addcoupon.blade.php
@@ -30,23 +30,17 @@
             <!-- 添加coupon -->
             <section class="m-b-20x">
                 <article class="font-size-md text-main p-a-10x"><strong>Promotion Code</strong></article>
-                <form id="infoForm" method="get" action="/cart/ordercheckout" method="get">
                     <fieldset>
                         <div class="warning-info flex text-warning flex-alignCenter text-left p-a-15x" hidden>
                             <i class="iconfont icon-caveat icon-size-md p-r-5x"></i>
                             <span class="font-size-sm"></span>
                         </div>
-                        <input class="form-control form-control-block p-a-15x font-size-sm" type="text" name="cps" placeholder="Enter your code" value="{{$cps}}">
+                        <input class="form-control form-control-block p-a-15x font-size-sm" type="text" name="coupon" placeholder="Enter your code" value="{{$cps}}">
                     </fieldset>
                     <div class="navbar-fixed-bottom bg-white p-a-15x">
                         <div class="btn btn-primary btn-block btn-sm" data-role="submit">Apply</div>
                     </div>
-                    @if(isset($input) && !empty($input))
-                        @foreach($input as $name=>$value)
-                            <input type="hidden" name="{{$name}}" value="{{$value}}">
-                        @endforeach
-                    @endif
-                </form>
+
             </section>
         </div>
     </div>
@@ -54,6 +48,15 @@
     <div class="loading loading-screen loading-switch loading-hidden">
         <div class="loader loader-screen"></div>
     </div>
+<form id="infoForm" method="get" action="/cart/ordercheckout" method="get">
+    @if(isset($input) && !empty($input))
+        <input type="hidden" name="cps" value="">
+        @foreach($input as $name=>$value)
+            <input type="hidden" name="{{$name}}" value="{{$value}}">
+        @endforeach
+    @endif
+</form>
+
 </body>
 <script src="/scripts/vendor.js"></script>
 
