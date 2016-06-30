5c492f9535469613b2efb450dc6c8ebf9b0d4e63
zhangtao <250574087@qq.com>
2016-06-30 17:37:01 +0800

    setting-Payment Method 界面，当无支付方式时，edit按钮不显示。

--- resources/views/shopping/paymentmethod.blade.php
+++ resources/views/shopping/paymentmethod.blade.php
@@ -28,7 +28,9 @@
         <section>
             <article class="p-x-15x p-y-10x flex flex-fullJustified flex-alignCenter">
                 <span class="font-size-md text-main"><strong>Payment Methods</strong></span>
-                <a class="btn btn-primary-outline btn-sm" id="payment-edit">Edit</a>
+                @if(count($methodlist['Card']) > 0)
+                    <a class="btn btn-primary-outline btn-sm" id="payment-edit">Edit</a>
+                @endif
 
                 <!-- 修改状态 -->
                 <!--<a class="btn btn-primary btn-sm" href="#">Done</a>-->
