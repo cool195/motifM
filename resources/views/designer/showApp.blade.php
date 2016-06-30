2ebd85ee67394e0bcac8c845ece4037db0c7ba3e
SuperJu <juchao1989@foxmail.com>
2016-06-30 14:18:09 +0800

    update

--- resources/views/designer/showApp.blade.php
+++ resources/views/designer/showApp.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <title>Designer Detail</title>
+    <title>{{$designer['nickname']}}</title>
     <link rel="icon" href="/favicon.ico">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
