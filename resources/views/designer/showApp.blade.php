1d15f770efd849088a230dd5dc250763c08745ad
SuperJu <juchao1989@foxmail.com>
2016-07-04 16:38:55 +0800

    update

--- resources/views/designer/showApp.blade.php
+++ resources/views/designer/showApp.blade.php
@@ -70,6 +70,13 @@
                                  srcset="/images/designer/youtube@2x.png 2x,/images/designer/youtube@3x.png 3x">
                         </a>
                     @endif
+                        @if(!empty($designer['snapchat_link']))
+                            <a href="motif://o.c?a=outurl&url={{$designer['snapchat_link']}}" target="_blank"
+                               class="p-r-20x">
+                                <img src="/images/designer/snapchat.png"
+                                     srcset="/images/designer/ins@2x.png 2x,/images/designer/ins@3x.png 3x">
+                            </a>
+                        @endif
                     @if(!empty($designer['facebook_link']))
                         <a href="motif://o.c?a=outurl&url={{$designer['facebook_link']}}" target="_blank"
                            class="p-r-20x">
