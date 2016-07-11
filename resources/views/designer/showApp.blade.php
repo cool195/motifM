83ec243cab9a6f1519bf44b1443ae303709adeb2
zhangtao <250574087@qq.com>
2016-07-11 19:33:26 +0800

    统一文案

--- resources/views/designer/showApp.blade.php
+++ resources/views/designer/showApp.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>{{$designer['nickname']}}</title>
+    <title>Designer</title>
     @include('head')
     <link rel="stylesheet" href="/styles/designerDetail.css">
 </head>
