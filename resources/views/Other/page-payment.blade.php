405687325182b4df46d20fcca21e16cba6d5cb7c
lhyin <993406018@qq.com>
2016-07-12 10:10:49 +0800

    update 更新 payment 文案

--- resources/views/Other/page-payment.blade.php
+++ resources/views/Other/page-payment.blade.php
@@ -22,41 +22,35 @@
                     @endif
                             <!-- 物流、退货、支付 说明 -->
                     <section class="m-b-20x p-b-20x reserve-height">
-                        <article class="font-size-md text-main p-x-15x p-y-10x"><strong>Payments</strong>
+                        <article class="font-size-md text-main p-x-15x p-y-10x"><strong>Payment</strong>
                         </article>
                         <div class="bg-white m-b-10x">
                             <hr class="hr-base m-y-0">
                             <div class="p-a-15x font-size-sm text-primary">
                                 <p class="m-b-15x">At the checkout, you can proceed as a guest or create an online
-                                    account with us
-                                    once you have placed your order. By creating an online account, you can enjoy a
-                                    quicker checkout
-                                    process in the future and save your delivery and payment details. If you are an
-                                    existing
-                                    customer, you will be able to log in to your account.
+                                    account with us once you have placed your order. By creating an online account, you
+                                    can enjoy a quicker checkout process in the future and save your delivery and
+                                    payment details. If you are an existing customer, you will be able to log in to your
+                                    account.
                                 </p>
                                 <div>
-                                    <p class="m-b-15x"><strong>METHODS OF PAYMENT</strong></p>
+                                    <p class="m-b-15x"><strong>METHOD OF PAYMENT</strong></p>
                                     <p class="m-b-15x">
-                                        <strong>1) Credit card:</strong> We accept the following cards for payment of
-                                        purchases made
-                                        online
+                                        <strong>PayPal:</strong> We accept PayPal as one method of payment. After
+                                        clicking on PayPal
+                                        Express Checkout, you will be redirected to PayPal where you can either create
+                                        an account or login to an existing account and arrange payment of your order.
+                                        You will be redirected to the website once your order is received.
                                     </p>
-                                    <p><img class="img-fluid" src="/images/payment/cards.png"
-                                            srcset="/images/payment/cards@2x.png 2x, /images/payment/cards@3x.png 3x"
-                                            alt=""></p>
-                                    <p class="m-b-15x">Payment will be taken from your credit or debit card as soon as
-                                        you have placed your order. To ensure safe shopping, we are GoDaddy SSL
-                                        certified which will automatically create an encrypted connection with
-                                        customer’s browser and protect all the sensitive information.
-                                    </p>
-
                                     <p class="m-b-15x">
-                                        <strong>2) PayPal:</strong> We also accept PayPal as a method of payment. If you
-                                        select this option in the checkout, you will be redirected to PayPal where you
-                                        can either create an account or login to an existing account and arrange payment
-                                        of your order. You will be redirected to the website once your order is
-                                        received.
+                                        <strong>eCheck:</strong> We accept eCheck as well. With an eCheck, we could
+                                        receive the payment in 5-7 business days depending on the regulations of your
+                                        bank. Therefore, we consider the date of receiving the payment as your order
+                                        date. We will process your order only after we receive your payment.
+                                    </p>
+                                    <p class="m-b-15x">
+                                        <strong>Coupon Code:</strong> Please fill in the Coupon Code you have in the
+                                        discount code box while making payment.
                                     </p>
 
                                     <p class="m-b-15x"><strong>Declined PayPal Transactions</strong></p>
@@ -64,26 +58,19 @@
                                         There are several reasons your payment may have been declined by PayPal:
                                     </p>
                                     <p class="m-b-15x">
-                                        1) Suspicion of fraud or incorrect billing information<br>
-                                        2) Invalid credit card or account linked to PayPal (or none linked)<br>
+                                        1) Suspicion of fraud or incorrect billing information<br/>
+                                        2) Invalid credit card or account linked to PayPal (or none linked)<br/>
                                         3) Insufficient funds
                                     </p>
                                     <p class="m-b-15x">
                                         For more information, please log in to your PayPal account, or contact PayPal
-                                        Customer
-                                        Support.
+                                        Customer Support.
                                     </p>
 
                                     <p class="m-b-15x">
-                                        <strong>3) Coupon Code:</strong> Please fill in the Coupon Code you have in the
-                                        box while
-                                        making payment.
-                                    </p>
-                                    <p class="m-b-0">If you are experiencing difficulties with your payment or order
-                                        please contact
+                                        If you are experiencing difficulties with your payment or order please contact
                                         our Customer Service Team via Live Chat or email <a
-                                                href="mailto:service@motif.me"
-                                                class="text-underLine">service@motif
+                                                href="mailto:service@motif.me" class="text-underLine">service@motif
                                             .me</a>.
                                     </p>
                                 </div>
