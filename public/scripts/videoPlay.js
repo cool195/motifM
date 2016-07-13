19ad5ab527b2fc3d9bcd5f0c00b657afe24a8e91
lhyin <993406018@qq.com>
2016-07-13 14:19:19 +0800

    update 更新视频在iOS 下停止播放，在安卓下正常播放

--- public/scripts/videoPlay.js
+++ public/scripts/videoPlay.js
@@ -5,22 +5,26 @@
 /* eslint-disable */
 'use strict';
 
-// 设置 视频默认播放 和 关闭音量 和 视频继续播放
-function onPlayerReady(event) {
-    event.target.playVideo();
-    event.target.mute();
-}
-
-// 视频播放失败
-function onPlayerError(event) {
-    event.target.playVideo();
-}
 // youtube 视频播放
 // 视频比例
 var MediaScale = 9 / 16;
 
 var Width = $(window).width(),
-    MediaHeight = Width * MediaScale;
+    MediaHeight = Width * MediaScale,
+    autoplay = 0;
+switch (switchDevice()) {
+    case 1:
+        autoplay = 0;
+        break;
+    case 0:
+        autoplay = 1;
+        break;
+    case -1:
+        autoplay = 1;
+        break;
+    default:
+        break;
+}
 if ($('#ytplayer').length > 0) {
     // 初始化 外边框尺寸
     $('.designer-media').css('height', MediaHeight);
@@ -41,12 +45,38 @@ function onYouTubePlayerAPIReady() {
         height: MediaHeight,
         width: Width,
         videoId: PlayId,
-        autoplay: 1,
+        //playerVars: {'autoplay': 0},
         events: {
             'onReady': onPlayerReady,
             'onError': onPlayerError
         }
     });
+}
+
+// 设置 视频默认播放 和 关闭音量 和 视频继续播放
+function onPlayerReady(event) {
+    if (autoplay == 0) {
+        event.target.stopVideo();
+    } else {
+        event.target.playVideo();
+    }
+    event.target.mute();
+}
+
+// 视频播放失败
+function onPlayerError(event) {
+    event.target.playVideo();
+}
+
+function switchDevice() {
+    var Agent = navigator.userAgent;
+    if (/iPhone/i.test(Agent)) {
+        return 1;
+    } else if (/Android/i.test(Agent) || /Linux/i.test(Agent)) {
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
 //# sourceMappingURL=videoPlay.js.map
