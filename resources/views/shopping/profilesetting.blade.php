f8f74a42f542b84e9c8a9ff814e01d03c4997b0d
zhangtao <250574087@qq.com>
2016-07-13 19:47:56 +0800

    隐藏Payment Method

--- resources/views/shopping/profilesetting.blade.php
+++ resources/views/shopping/profilesetting.blade.php
@@ -33,11 +33,11 @@
                         <span>Shipping Address</span>
                         <i class="iconfont icon-arrow-right icon-size-xm text-common"></i>
                     </a>
-                    <hr class="hr-base m-a-0">
+{{--                    <hr class="hr-base m-a-0">
                     <a class="flex flex-alignCenter flex-fullJustified font-size-sm text-primary p-a-15x" href="/braintree">
                         <span>Payment Method</span>
                         <i class="iconfont icon-arrow-right icon-size-xm text-common"></i>
-                    </a>
+                    </a>--}}
                 </aside>
             </section>
 <!-- 页脚 功能链接 start-->
