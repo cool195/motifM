cc25fd04ca3b24502558b155ba825d056339dde6
SuperJu <juchao1989@foxmail.com>
2016-07-14 12:34:42 +0800

    update

--- resources/views/designer/showApp.blade.php
+++ resources/views/designer/showApp.blade.php
@@ -26,7 +26,7 @@
                         </div>
                     </div>
                 @else
-                    <img src="{{env('APP_Api_Image')}}/n0/{{$designer['img_video_path']}}" class="designer-realImg" hidden>
+                    {{--<img src="{{env('APP_Api_Image')}}/n0/{{$designer['img_video_path']}}" class="designer-realImg" hidden>--}}
                     <img class="img-fluid img-lazy designer-Img" data-original="{{env('APP_Api_Image')}}/n0/{{$designer['img_video_path']}}" src="/images/designer/bg-designer@750x550.png">
                 @endif
             </div>
