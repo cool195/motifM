18fb3b06b4380d8ffdb4be57c7bc652e49a1b1e3
zhangtao <250574087@qq.com>
2016-07-14 11:11:15 +0800

    forget发送邮件加token

--- app/Http/Controllers/Shopping/UserController.php
+++ app/Http/Controllers/Shopping/UserController.php
@@ -208,7 +208,7 @@ class UserController extends ApiController
             'cmd' => "forgetpwd",
             'uuid' => @$_COOKIE['uid'],
             'email' => $request->input('email'),
-            //'token' => Session::get('user.token'),
+            'token' => self::Token,
             //'pin' => Session::get('user.pin'),
         );
         $result = $this->request('openapi', self::API_SYSTEM, self::API_SERVICE, $params);
