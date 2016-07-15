f7ef8d63110c1ae4518dce5a1f6a7f7676b31b19
SuperJu <juchao1989@foxmail.com>
2016-07-15 18:32:32 +0800

    update

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,6 +2,7 @@
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 <meta name="apple-itunes-app" content="app-id=432274380">
+<meta http-equiv="Access-Control-Allow-Origin" content="*">
 <link rel="icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <!-- Place favicon.ico in the root directory -->
