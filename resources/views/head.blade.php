dc545be586136ebcaafcccbf419079de820d5895
SuperJu <juchao1989@foxmail.com>
2016-07-21 15:55:50 +0800

    update

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,7 +1,11 @@
 <meta charset="UTF-8">
-<meta name="description" content="">
+<meta name="title" content="MOTIF Exclusive Fashion Accessories Designed by the World’s Top Fashion Bloggers, Instagrammers and Digital Influencers">
+<meta name="description" content="Your style is unique and cutting edge - your fashion should be too.Exclusive, limited edition accessories designed by the world’s top fashion bloggers, Instagrammers and digital influencers.">
 <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 <meta name="apple-itunes-app" content="app-id=1125850409">
+<meta property="og:image" content="http://cdn.m.motif.me/apple-touch-icon.png" />
+<meta property="og:title" content="MOTIF Exclusive Fashion Accessories Designed by the World’s Top Fashion Bloggers, Instagrammers and Digital Influencers" />
+<meta property="og:url" content="http://m.motif.me" />
 <meta http-equiv="Access-Control-Allow-Origin" content="*">
 <link rel="icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
