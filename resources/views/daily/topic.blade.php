df04e99744a589a01aab755cc58a60a9e6acde60
zhangtao <250574087@qq.com>
2016-08-05 15:06:41 +0800

    topic,topicApp曝光，点击埋点更新

--- resources/views/daily/topic.blade.php
+++ resources/views/daily/topic.blade.php
@@ -51,7 +51,7 @@
                                 @foreach($value['spus'] as $spu)
                                     <div class="p-x-15x p-y-10x">
                                         <a data-clk='http://clk.motif.me/log.gif?t=daily.200001&m=H5_M2016-1&pin={{Session::get('user.pin')}}&uuid={{Session::get('user.uuid')}}&v={"action":1,"skipType":1,"skipId":{{$spu}},"topicId":{{$topicID}},"expid":0,"ver":"1.0.1","src":"H5"}'
-                                           href="/detail/{{$spu}}">
+                                           data-link="/detail/{{$spu}}" href="javascript:void(0)">
                                             <img class="img-fluid img-lazy"
                                                  data-original="{{env('APP_Api_Image')}}/n1/{{$topic['spuInfos'][$spu]['spuBase']['main_image_url']}}"
                                                  src="{{env('CDN_Static')}}/images/product/bg-product@750.png"
@@ -74,7 +74,7 @@
                                         @foreach($value['spus'] as $spu)
                                             <div class="col-xs-6">
                                                 <a data-clk='http://clk.motif.me/log.gif?t=daily.200001&m=H5_M2016-1&pin={{Session::get('user.pin')}}&uuid={{Session::get('user.uuid')}}&v={"action":1,"skipType":1,"skipId":{{$spu}},"topicId":{{$topicID}},"expid":0,"ver":"1.0.1","src":"H5"}'
-                                                   href="/detail/{{$spu}}">
+                                                   data-link=/detail/{{$spu}}" href="javascript:void(0)">
                                                     <div class="p-t-10x productList-item m-b-0">
                                                         <div class="image-container">
                                                             <img class="img-thumbnail img-lazy"
@@ -118,20 +118,6 @@
             effect: 'fadeIn'
         });
     });
-
-    $(document).ready(function() {
-        $.ajax({
-            url: $('[data-impr]').data('impr'),
-            type: "GET"
-        });
-
-        $('[data-clk]').click(function() {
-            $.ajax({
-                url: $(this).data('clk'),
-                type: "GET"
-            });
-        })
-    })
 </script>
 @include('global')
 </html>
--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -1,1 +1,23 @@
+<script>
+    $(document).ready(function() {
+        $('[data-clk]').click(function() {
+            $this = $(this);
+            if(undefined !== $this.data('link')){
+                $.ajax({
+                    url: $this.data('clk'),
+                    type: "GET"
+                });
+                setTimeout(function() {
+                    window.location.href = $this.data('link');
+                }, 100);
+            }
+        })
+
+        $.ajax({
+            url: $('[data-impr]').data('impr'),
+            type: "GET"
+        });
+    })
+</script>
+
 <script src="http://clk.motif.me/wl.js"></script>
