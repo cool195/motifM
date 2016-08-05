ee2df7c10eacd1b2bf5ff84b61be68917548fe18
zhangtao <250574087@qq.com>
2016-08-05 17:31:42 +0800

    shoppingList页面点击与曝光埋点

--- public/scripts/shoppingList.js
+++ public/scripts/shoppingList.js
@@ -153,6 +153,21 @@
                             container: $('#tabs-container'),
                             effect: 'fadeIn'
                         });
+
+                        //给模板a标签绑定事件
+                        $('[data-clk]').unbind('click');
+                        $('[data-clk]').bind('click', function() {
+                            var $this = $(this);
+                            if(undefined !== $this.data('link')){
+                                $.ajax({
+                                    url: $this.data('clk'),
+                                    type: "GET"
+                                });
+                                setTimeout(function() {
+                                    window.location.href = $this.data('link');
+                                }, 100);
+                            }
+                        })
                     }
                 }
             })
