51fe950e7be94c4d2681bcf3884dbbbd1b7b2dfc
SuperJu <juchao1989@foxmail.com>
2016-08-11 11:11:11 +0800

    update

--- resources/views/daily/topicApp.blade.php
+++ resources/views/daily/topicApp.blade.php
@@ -81,7 +81,7 @@
                                             <div class="col-xs-6 p-a-0">
                                                 <div class="bg-white topic-product-item productList-item">
                                                     <a data-clk='http://clk.motif.me/log.gif?t=daily.200001&m=H5_M2016-1&pin={{Session::get('user.pin')}}&uuid={{Session::get('user.uuid')}}&v={"action":1,"skipType":1,"skipId":{{$spu}},"topicId":{{$topicID}},"expid":0,"ver":"1.0.1","src":"H5"}'
-                                                       data-link="/detail/{{$spu}}" href="javascript:void(0)">
+                                                       data-link="motif://o.c?a=pd&spu={{$spu}}" href="javascript:void(0)">
                                                         <div class="image-container">
                                                             <img class="img-fluid img-lazy"
                                                                  data-original="{{env('APP_Api_Image')}}/n2/{{$topic['spuInfos'][$spu]['spuBase']['main_image_url']}}"
