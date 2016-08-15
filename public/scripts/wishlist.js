46f2df989aff57f4c829f19ecb685743459af4d6
lhyin <993406018@qq.com>
2016-08-15 17:19:54 +0800

    update 更新 wishlist

--- public/scripts/wishlist.js
+++ public/scripts/wishlist.js
@@ -2,6 +2,16 @@
 /* global jQuery */
 
 (function ($) {
+    // 加载动画显示
+    function loadingShow() {
+        $('.wishloading').show();
+    }
+
+    // 加载动画隐藏
+    function loadingHide() {
+        $('.wishloading').hide();
+    }
+
     // loading 打开
     function openLoading() {
         $('.loading').toggleClass('loading-hidden');
@@ -26,7 +36,7 @@
     // 删除购物车商品 提示框
     var wishModal = $('[data-remodal-id="modal"]').remodal(Options);
 
-    $('.delwish').click(function (e) {
+    $('#wishContainer').on('click', '.delwish', function (e) {
         // 暂存数据 to modal , 为拼接 ajax url 做准备
         $('#wishDialog').data('spu', $(e.target).data('spu'));
         wishModal.open();
@@ -58,12 +68,102 @@
     });
 
     // 图片延迟加载
-    $(function(){
+    $(function () {
         $('img.img-lazy').lazyload({
             threshold: 200,
             effect: 'fadeIn'
         });
     });
+
+    // ajax 获取 wishlist 列表
+    function getWishList() {
+        //  $wishContainer 列表容器
+        //  Wishpagenum 产品当前页码数
+        var $wishContainer = $('#wishContainer'),
+            Wishpagenum = $wishContainer.data('wishpagenum');
+        // 判断是否还有数据要加载
+        if (Wishpagenum === -1) {
+            return;
+        }
+
+        // 判断当前选项卡是否在加载中
+        if ($wishContainer.data('loading') === true) {
+            return;
+        } else {
+            $wishContainer.data('loading', true);
+        }
+        var NextWishNum = ++Wishpagenum;
+
+        loadingShow();
+        $.ajax({
+                url: '/wish',
+                data: {
+                    num: NextWishNum,
+                    size: 10,
+                    ajax: 1
+                }
+            })
+            .done(function (data) {
+                if (data.data === null || data.data === '') {
+                    $wishContainer.data('wishpagenum', -1);
+                } else if (data.data.list === null || data.data.list === '' || data.data.list === undefined || data.data.list.length === 0) {
+                    $wishContainer.data('wishpagenum', -1);
+                } else {
+                    // 遍历模板 插入页面
+                    appendWishList('tpl-wishlist', data.data);
+                    // 页数 +1
+                    $wishContainer.data('wishpagenum', Wishpagenum);
+
+                    // 图片延迟加载
+                    $('img.img-lazy').lazyload({
+                        threshold: 200,
+                        effect: 'fadeIn'
+                    });
+                }
+            })
+            .always(function () {
+                $wishContainer.data('loading', false);
+                loadingHide();
+            });
+
+    }
+
+    // 将数据插入到模板中
+    function appendWishList(tpl, WishList) {
+        var TplHtml = template(tpl, WishList);
+        // 把 字符串 转义成 HTML
+        var StageCache = $.parseHTML(TplHtml);
+        // 将 html 插入页面相应位置
+        $('#wishContainer').append(StageCache);
+    }
+
+    // 为页面绑定 滚动条事件
+    $(document).ready(function () {
+        // 首次加载
+        getWishList();
+        $(window).scroll(function () {
+            $('img.img-lazy').each(function () {
+                var Src = $(this).attr('src'),
+                    Original = $(this).attr('data-original');
+                if (Src === Original) {
+                    $(this).removeClass('img-lazy');
+                }
+            });
+            pullLoading();
+        });
+    });
+
+    // 下拉加载
+    function pullLoading() {
+        // scrollCurrent    当前滚动距离
+        // scrollMax        最大滚动距离
+        var scrollCurrent = window.pageYOffset,
+            scrollMax = $(document).height() - $(window).height();
+        // 当页面在底部区域时, 触发加载事件
+        if (scrollCurrent !== scrollMax & scrollMax <= 300 + scrollCurrent) {
+            getWishList();
+        }
+    }
 
 })(jQuery);
 
--- resources/views/Other/wishlist.blade.php
+++ resources/views/Other/wishlist.blade.php
@@ -20,36 +20,10 @@
         <!-- wishlist 商品列表 -->
         <section class="reserve-height">
             <!-- 商品列表 -->
-            <aside class="wishList bg-white m-b-20x">
-                @foreach($data['list'] as $list)
-                <div class="wishlist-item p-a-15x" data-wishspu="{{$list['spu']}}">
-                    <div class="flex">
-                        <a href="/detail/{{$list['spu']}}">
-                            <div class="flex-fixedShrink">
-                                <img class="img-thumbnail img-lazy"
-                                     src="{{env('CDN_Static')}}/images/product/bg-product@70.png"
-                                     data-original="{{ env('APP_Api_Image').'/n2/'.$list['main_image_url']}}"
-                                     width="70" height="70">
-                            </div>
-                        </a>
-                        <div class="p-l-10x flex-width">
-                            <article class="flex flex-fullJustified wishlist-title">
-                                <h6 class="text-main font-size-md p-r-20x">
-                                    <strong>{{$list['main_title']}}</strong>
-                                </h6>
-                                <span class="text-primary font-size-sm flex-fixedShrink">
-                                    <i class="iconfont icon-cross icon-size-md text-common delwish" data-spu="{{$list['spu']}}"></i>
-                                </span>
-                            </article>
-                            <aside class="text-primary font-size-sm">
-                                <div>${{ number_format(( $list['skuPrice']['sale_price'] / 100), 2) }}</div>
-                            </aside>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
+            <aside id="wishContainer" class="wishList bg-white m-b-20x" data-loading="false" data-pagenum="0" data-wishpagenum="0">
+
             </aside>
-            <div class="loading" style="display: block">
+            <div class="loading wishloading" style="display: none">
                 <div class="loader"></div>
             </div>
         </section>
@@ -60,35 +34,35 @@
     </div>
 </div>
 
-{{--<template id="tpl-wishlist">--}}
-    {{--@{{ each list }}--}}
-    {{--<div class="wishlist-item p-a-15x" data-wishspu="{{$list['spu']}}">--}}
-        {{--<div class="flex">--}}
-            {{--<a href="/detail/{{$list['spu']}}">--}}
-                {{--<div class="flex-fixedShrink">--}}
-                    {{--<img class="img-thumbnail img-lazy"--}}
-                         {{--src="{{env('CDN_Static')}}/images/product/bg-product@70.png"--}}
-                         {{--data-original="{{ env('APP_Api_Image').'/n2/'.$list['main_image_url']}}"--}}
-                         {{--width="70" height="70">--}}
-                {{--</div>--}}
-            {{--</a>--}}
-            {{--<div class="p-l-10x flex-width">--}}
-                {{--<article class="flex flex-fullJustified wishlist-title">--}}
-                    {{--<h6 class="text-main font-size-md p-r-20x">--}}
-                        {{--<strong>{{$list['main_title']}}</strong>--}}
-                    {{--</h6>--}}
-                                    {{--<span class="text-primary font-size-sm flex-fixedShrink">--}}
-                                        {{--<i class="iconfont icon-cross icon-size-md text-common delwish" data-spu="{{$list['spu']}}"></i>--}}
-                                    {{--</span>--}}
-                {{--</article>--}}
-                {{--<aside class="text-primary font-size-sm">--}}
-                    {{--<div>${{ number_format(( $list['skuPrice']['sale_price'] / 100), 2) }}</div>--}}
-                {{--</aside>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    {{--@{{ /each }}--}}
-{{--</template>--}}
+<template id="tpl-wishlist">
+    @{{ each list }}
+    <div class="wishlist-item p-a-15x" data-wishspu="@{{ $value.spu }}">
+        <div class="flex">
+            <a href="/detail/@{{ $value.spu }}">
+                <div class="flex-fixedShrink">
+                    <img class="img-thumbnail img-lazy"
+                         src="{{env('CDN_Static')}}/images/product/bg-product@70.png"
+                         data-original="{{env('APP_Api_Image')}}/n1/@{{ $value.main_image_url }}"
+                         width="70" height="70">
+                </div>
+            </a>
+            <div class="p-l-10x flex-width">
+                <article class="flex flex-fullJustified wishlist-title">
+                    <h6 class="text-main font-size-md p-r-20x">
+                        <strong>@{{ $value.main_title }}</strong>
+                    </h6>
+                                    <span class="text-primary font-size-sm flex-fixedShrink">
+                                        <i class="iconfont icon-cross icon-size-md text-common delwish" data-spu="@{{ $value.spu }}"></i>
+                                    </span>
+                </article>
+                <aside class="text-primary font-size-sm">
+                    <div>$@{{ ($value.skuPrice.sale_price/100).toFixed(2) }}</div>
+                </aside>
+            </div>
+        </div>
+    </div>
+    @{{ /each }}
+</template>
 
 <!-- 删除将要购买的商品 -->
 <div class="remodal remodal-md modal-content" data-remodal-id="modal" id="wishDialog" data-spu="">
