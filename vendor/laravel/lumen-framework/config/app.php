db58203b0f0c69af4549ab49eb18375671bd1203
SuperJu <juchao1989@foxmail.com>
2016-08-15 19:19:15 +0800

    update

--- vendor/laravel/lumen-framework/config/app.php
+++ vendor/laravel/lumen-framework/config/app.php
@@ -12,7 +12,7 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-    'version' => '2.1',
+    'version' => '2.2',
     'key' => env('APP_KEY', 'SomeRandomString!!!'),
 
     'cipher' => 'AES-256-CBC',
