8c6614572395c6bd4db4382d578ea389f915b744
SuperJu <juchao1989@foxmail.com>
2016-08-23 16:04:05 +0800

    update

--- app/Http/Controllers/Shopping/QianhaiController.php
+++ app/Http/Controllers/Shopping/QianhaiController.php
@@ -32,7 +32,7 @@ class QianhaiController extends ApiController
             'order_number' => $request->input('orderid'),
             'order_currency' => 'USD',
             'order_amount' => $request->input('totalPrice'),
-            'billing_firstName' => $this->replace(trim($addrData['data']['userAddr']['name']),["'",'"',">","<"],['&#039;','&quot;','&gt;','&lt;'],0),
+            'billing_firstName' => str_replace('<','&lt;',str_replace('>','&gt;',str_replace('"','&quot;',str_replace("'",'&#039;',trim($addrData['data']['userAddr']['name']))))),
             'billing_lastName' => 'N/A',
             'billing_email' => Session::get('user.login_email'),
             'billing_phone' => $addrData['data']['userAddr']['telephone'] ? $addrData['data']['userAddr']['telephone'] : 'N/A',
