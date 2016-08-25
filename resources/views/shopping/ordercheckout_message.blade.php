3d0a60655acaa86729693ad5ad0a18acb52771fc
SuperJu <juchao1989@foxmail.com>
2016-08-25 14:45:04 +0800

    update

--- resources/views/shopping/ordercheckout_message.blade.php
+++ resources/views/shopping/ordercheckout_message.blade.php
@@ -22,7 +22,7 @@
                 <form id="infoForm" action="/cart/ordercheckout" method="get">
                     <fieldset>
                         <div class="message-info">
-                            <textarea class="form-control form-control-block p-a-15x font-size-sm" name="remark" id="messageContent"  placeholder="Message to us" rows="12" data-length="1000">{{$remark}}</textarea>
+                            <textarea class="form-control form-control-block p-a-15x font-size-sm" name="remark" id="messageContent"  placeholder="Special Note" rows="12" data-length="1000">{{$remark}}</textarea>
                             <span class="message-wordNumber font-size-sm text-primary"><span id="wordNum">0</span>/1000</span>
                         </div>
                     </fieldset>
