d48536312e928c254e40c8197d33269a0238f9e1
lhyin <993406018@qq.com>
2016-08-30 14:28:14 +0800

    update

--- public/scripts/videoPlay.js
+++ public/scripts/videoPlay.js
@@ -40,6 +40,7 @@ if ($('.ytplayer').length > 0) {
 
 }
 var player;
+var html5Player;
 function onYouTubeIframeAPIReady() {
     var PlayId = $('.ytplayer').data('playid');
     player = new YT.Player(PlayId, {
@@ -48,60 +49,71 @@ function onYouTubeIframeAPIReady() {
         videoId: PlayId,
         playerVars: {'autoplay': 1, 'controls': 2, 'showinfo': 0, 'fs': 0, 'playsinline': 1},
         events: {
-            //'onReady': onPlayerReady,
+            'onReady': onPlayerReady
             //'onError': onPlayerError,
             //'onStateChange': onPlayerStateChange
         }
     });
 }
 
-$('.bg-player').on('click', function (player) {
-    //var PlayId = $(this).siblings('.ytplayer').data('playid');
-    //player = new YT.Player(PlayId, {
-    //    height: MediaHeight,
-    //    width: Width,
-    //    videoId: PlayId,
-    //    playerVars: {'autoplay': 1, 'controls': 2, 'showinfo': 0, 'fs': 0, 'playsinline': 1},
-    //    events: {
-    //        'onReady': onPlayerReady,
-    //        'onError': onPlayerError,
-    //        'onStateChange': onPlayerStateChange
-    //    }
-    //});
-
-    //$ClickPlayer = $(this);
-    $(this).css('display', 'none');
-    $(this).children('.bg-img').hide();
-    $(this).children('.btn-beginPlayer').hide();
-    $(this).siblings('.btn-morePlayer').show();
-    //$(this).parents('.player-item').addClass('active');
-    player.playVideo();
-});
+//$('.bg-player').on('click', function (player) {
+//    //var PlayId = $(this).siblings('.ytplayer').data('playid');
+//    //player = new YT.Player(PlayId, {
+//    //    height: MediaHeight,
+//    //    width: Width,
+//    //    videoId: PlayId,
+//    //    playerVars: {'autoplay': 1, 'controls': 2, 'showinfo': 0, 'fs': 0, 'playsinline': 1},
+//    //    events: {
+//    //        'onReady': onPlayerReady,
+//    //        'onError': onPlayerError,
+//    //        'onStateChange': onPlayerStateChange
+//    //    }
+//    //});
+//
+//    //$ClickPlayer = $(this);
+//    $(this).css('display', 'none');
+//    $(this).children('.bg-img').hide();
+//    $(this).children('.btn-beginPlayer').hide();
+//    $(this).siblings('.btn-morePlayer').show();
+//    //$(this).parents('.player-item').addClass('active');
+//    player.playVideo();
+//});
 
 // 设置 视频默认播放 和 关闭音量 和 视频继续播放
 function onPlayerReady(event) {
     //if (autoplay == 0) {
     //    event.target.stopVideo();
     //} else {
-    setTimeout(event.target.playVideo(), 6000);
+    //setTimeout(event.target.playVideo(), 6000);
     //}
     //event.target.mute();
-}
+    console.log('chenggong');
 
-// 视频播放失败
-function onPlayerError(event) {
-    event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == 3 && !done) {
-        setTimeout(playVideo, 5000);
-        done = true;
+    if (event) {
+        html5Player = event.target;
     }
 }
-function playVideo() {
-    player.playVideo();
+
+
+// 视频播放失败
+//function onPlayerError(event) {
+//    event.target.playVideo();
+//}
+
+//var done = false;
+//function onPlayerStateChange(event) {
+//    if (event.data == 3 && !done) {
+//        setTimeout(playVideo, 5000);
+//        done = true;
+//    }
+//}
+function play() {
+    $('.bg-player').css('display', 'none');
+    $('.bg-player').children('.bg-img').hide();
+    $('.bg-player').children('.btn-beginPlayer').hide();
+    $('.bg-player').siblings('.btn-morePlayer').show();
+    //$(this).parents('.player-item').addClass('active');
+    html5Player.playVideo();
 }
 
 function switchDevice() {
