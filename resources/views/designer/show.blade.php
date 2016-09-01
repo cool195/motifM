b81510ac561afae1436444dea3c5bb04c492ba24
lhyin <993406018@qq.com>
2016-09-01 11:27:32 +0800

    update 商品列表中的横线  and 设计师主页预售商品样式问题

--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -272,23 +272,22 @@
                                                                              src="{{env('CDN_Static')}}/images/product/bg-product@336.png"
                                                                              data-original="{{env('APP_Api_Image')}}/n2/{{$product['spuInfos'][$spu]['spuBase']['main_image_url']}}"
                                                                              alt="{{$product['spuInfos'][$spu]['spuBase']['main_title']}}">
+                                                                        @if(1 == $product['spuInfos'][$spu]['spuBase']['sale_type'])
+                                                                            @if(!isset($product['spuInfos'][$spu]['skuPrice']['skuPromotion']) || $product['spuInfos'][$spu]['stockStatus']=='NO' || $product['spuInfos'][$spu]['spuBase']['isPutOn']==0)
+                                                                                <a data-link="/detail/{{$spu}}"
+                                                                                   data-clk='http://clk.motif.me/log.gif?t=designer.400001&m=H5_M2016-1&pin={{ Session::get('user.pin') }}&uuid={{ Session::get('user.uuid') }}&v={"action":1,"skipType":1,"skipId"{{$spu}},"expid":0,"index":{{$key}},"version":"1.0.1","ver":"9.2","src":"H5"}'
+                                                                                   href="javascript:void(0)">
+                                                                                    <div class="preorderSold-info">
+                                                                                        <span class="font-size-xs">SOLD OUT</span>
+                                                                                    </div>
+                                                                                </a>
+                                                                            @else
+                                                                                <span class="preorder-info font-size-xs">Limited Edition</span>
+                                                                            @endif
+
+                                                                        @endif
                                                                     </div>
                                                                 </a>
-
-                                                                @if(1 == $product['spuInfos'][$spu]['spuBase']['sale_type'])
-                                                                    @if(!isset($product['spuInfos'][$spu]['skuPrice']['skuPromotion']) || $product['spuInfos'][$spu]['stockStatus']=='NO' || $product['spuInfos'][$spu]['spuBase']['isPutOn']==0)
-                                                                        <a data-link="/detail/{{$spu}}"
-                                                                           data-clk='http://clk.motif.me/log.gif?t=designer.400001&m=H5_M2016-1&pin={{ Session::get('user.pin') }}&uuid={{ Session::get('user.uuid') }}&v={"action":1,"skipType":1,"skipId"{{$spu}},"expid":0,"index":{{$key}},"version":"1.0.1","ver":"9.2","src":"H5"}'
-                                                                           href="javascript:void(0)">
-                                                                            <div class="preorderSold-info">
-                                                                                <span class="font-size-xs">SOLD OUT</span>
-                                                                            </div>
-                                                                        </a>
-                                                                    @else
-                                                                        <span class="preorder-info font-size-xs">Limited Edition</span>
-                                                                    @endif
-
-                                                                @endif
 
                                                                 <div class="p-a-10x">
                                                                     <span>
