0c9b085573b08670d531d9f598b6743f60885bd9
lhyin <993406018@qq.com>
2016-09-06 18:17:19 +0800

    add invite

---
+++ resources/views/Other/invite.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Invite Code</title>
+</head>
+<body>
+@include('check.tagmanager')
+
+<script type="text/javascript">
+    function switchDevice() {
+        var Agent = navigator.userAgent;
+        if (/iPhone/i.test(Agent)) {
+            return 1;
+        } else if (/Android/i.test(Agent) || /Linux/i.test(Agent)) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    var Device = switchDevice();
+    if (Device === 1) {
+        window.location.href = "https://itunes.apple.com/us/app/id1125850409";
+    } else if (Device === 0) {
+        window.location.href = "https://play.google.com/store/apps/details?id=me.motif.motif";
+    } else {
+        window.location.href = "http://m.motif.me";
+    }
+
+</script>
+</body>
+</html>
