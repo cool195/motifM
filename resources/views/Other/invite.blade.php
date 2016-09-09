099c98bfdb006274e8ef3c09cca42426d2d1b9a4
lhyin <993406018@qq.com>
2016-09-09 18:34:52 +0800

    update d/invite 页面

--- resources/views/Other/invite.blade.php
+++ resources/views/Other/invite.blade.php
@@ -2,9 +2,21 @@
 <html lang="en">
 <head>
     <title>Invite Code</title>
+    <meta property="og:image" content="/images/background/invite.jpg"/>
+    @include('head')
+    <link rel="stylesheet" href="{{env('CDN_Static')}}/styles/login.css{{'?v='.config('app.version')}}">
 </head>
 <body>
 @include('check.tagmanager')
+<img src="/images/background/invite.jpg" alt="" hidden>
+
+<div class="invite-container">
+    <div class="text-center">
+        <div class="m-b-20x p-b-10x"><span class="invite-arrow"></span></div>
+        <div class="p-x-20x p-y-10x bg-primary font-size-sm" onclick="invite();"><span class="p-x-10x">DOWNLOAD MOTIF</span>
+        </div>
+    </div>
+</div>
 
 <script type="text/javascript">
     function switchDevice() {
@@ -18,13 +30,15 @@
         }
     }
 
-    var Device = switchDevice();
-    if (Device === 1) {
-        window.location.href = "https://itunes.apple.com/us/app/id1125850409";
-    } else if (Device === 0) {
-        window.location.href = "https://play.google.com/store/apps/details?id=me.motif.motif";
-    } else {
-        window.location.href = "http://m.motif.me";
+    function invite() {
+        var Device = switchDevice();
+        if (Device === 1) {
+            window.location.href = "https://itunes.apple.com/us/app/id1125850409";
+        } else if (Device === 0) {
+            window.location.href = "https://play.google.com/store/apps/details?id=me.motif.motif";
+        } else {
+            window.location.href = "http://m.motif.me";
+        }
     }
 
 </script>
