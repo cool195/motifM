e07a49e6edd1ba1f125bd78549995cf71bdb24f2
SuperJu <juchao1989@foxmail.com>
2016-09-09 19:25:15 +0800

    update

--- resources/views/Other/invite.blade.php
+++ resources/views/Other/invite.blade.php
@@ -4,7 +4,6 @@
     <title>Invite Code</title>
     <meta property="og:url" content="http://test.m.motif.me/d/invite/asdf">
     @include('head')
-    <meta property="og:url" content="http://test.m.motif.me/d/invite/asdf">
     <meta property="og:image" content="{{env('CDN_Static')}}/images/background/invite.jpg"/>
     <link rel="stylesheet" href="{{env('CDN_Static')}}/styles/login.css{{'?v='.config('app.version')}}">
 </head>
