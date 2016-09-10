09597ca76e2831ab55e6e4efff2c5c271a8df390
SuperJu <juchao1989@foxmail.com>
2016-09-10 14:44:01 +0800

    update

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,4 @@
-<meta charset="UTF-8">
+<meta charset="en">
 <meta name="keywords" content="fashion,style,shop,accessory,jewelry,watch,blogger,Instagram,designer,limited,edition,ecommerce,buy">
 <meta name="description" content="Your style is unique and cutting edge - your fashion should be too.Exclusive, limited edition accessories designed by the world’s top fashion bloggers, Instagrammers and digital influencers.">
 <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
@@ -6,8 +6,6 @@
 <meta http-equiv="Access-Control-Allow-Origin" content="*">
 <link rel="icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-<!-- Place favicon.ico in the root directory -->
-
 <link rel="stylesheet" href="{{env('CDN_Static')}}/styles/vendor.css{{'?v='.config('app.version')}}">
 <script src="{{env('CDN_Static')}}/scripts/vendor/modernizr.js{{'?v='.config('app.version')}}"></script>
 <script src="{{env('CDN_Static')}}/scripts/vendor/fastclick.js{{'?v='.config('app.version')}}"></script>
