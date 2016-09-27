f73be8b4bf0d09d56f1bd664f36eb4f5639bf58d
SuperJu <juchao1989@foxmail.com>
2016-09-27 13:46:55 +0800

    update

--- resources/views/designer/skipDesigner.blade.php
+++ resources/views/designer/skipDesigner.blade.php
@@ -7,31 +7,6 @@
 @include('check.tagmanager')
 <script type="text/javascript">
 
-
-    function ios() {
-        var ifr = document.createElement("iframe");
-        ifr.src = "motif://o.c?a=url&url=http://m.motif.me/designer/92";
-        /***打开app的协议，有ios同事提供***/
-        ifr.style.display = "none";
-        document.body.appendChild(ifr);
-        window.setTimeout(function () {
-            document.body.removeChild(ifr);
-            window.location.href = "https://control.kochava.com/v1/cpi/click?campaign_id=komotif-kvced5a25acca5c8c39f&network_id=5033&device_id=device_id&site_id=1";
-            /***下载app的地址***/
-        }, 3000)
-    }
-
-
-    function android() {
-        //window.location = "motif://o.c?a=url&url=http://m.motif.me/designer/92";
-        /***打开app的协议，有安卓同事提供***/
-        window.setTimeout(function () {
-            window.location.href = "motif://o.c?a=url&url=http://m.motif.me/designer/92";
-            /***打开app的协议，有安卓同事提供***/
-        }, 3000);
-    }
-
-
     function switchDevice() {
         var Agent = navigator.userAgent;
         if (/iPhone/i.test(Agent)) {
@@ -45,11 +20,9 @@
     //
     var Device = switchDevice();
     if (Device === 1) {
-        ios()
-        //window.location.href = "https://control.kochava.com/v1/cpi/click?campaign_id=komotif-kvced5a25acca5c8c39f&network_id=5033&device_id=device_id&site_id=1";
+        window.location.href = "https://control.kochava.com/v1/cpi/click?campaign_id=komotif-kvced5a25acca5c8c39f&network_id=5033&device_id=device_id&site_id=1";
     } else if (Device === 0) {
-        android()
-        //window.location.href = "https://c89mm.app.goo.gl/Y2QC";
+        window.location.href = "https://c89mm.app.goo.gl/Y2QC";
     } else {
         window.location.href = "http://m.motif.me";
     }
