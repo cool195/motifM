60aa7b80bf666f164aec83a870386ce1af7c5119
SuperJu <juchao1989@foxmail.com>
2016-09-29 11:13:44 +0800

    update

--- resources/views/designer/downapp.blade.php
+++ resources/views/designer/downapp.blade.php
@@ -22,13 +22,18 @@
     var Device = switchDevice();
     var url = '';
     if (Device === 1) {
-        url = "https://control.kochava.com/v1/cpi/click?campaign_id=komotif-kvced5a40b7b7adfe749&network_id=5033&device_id=device_id&site_id=1";
+        window.location.href = "motif://o.c?a=url&url=http://m.motif.me/designer/99";
+        setTimeout(function() {
+            window.location.href = "http://m.motif.me/designer/99";
+        }, 2000);
     } else if (Device === 0) {
-        url = "https://c89mm.app.goo.gl/0XdJ";
+        window.location.href = "motif://o.c?a=url&url=http://m.motif.me/designer/99";
+        setTimeout(function() {
+            window.location.href = "http://m.motif.me/designer/99";
+        }, 2000);
     } else {
-        url = "http://m.motif.me/designer/99";
+        window.location.href = "http://m.motif.me/designer/99";
     }
-    window.location.href = url
 </script>
 </body>
 </html>
