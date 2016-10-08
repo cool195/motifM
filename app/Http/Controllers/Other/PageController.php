8d73ae31ab323a684fc7c0ab3761557b685b78e4
SuperJu <juchao1989@foxmail.com>
2016-10-08 10:08:43 +0800

    update

--- app/Http/Controllers/Other/PageController.php
+++ app/Http/Controllers/Other/PageController.php
@@ -104,15 +104,18 @@ class PageController extends ApiController
             'pin' => Session::get('user.pin'),
         );
         $result = $this->request('openapi', '', 'user', $params);
-        return view('Other.invite-friend',['code'=>$result['data']['invite_code']]);
+        return view('Other.invite-friend', ['code' => $result['data']['invite_code']]);
     }
 
-    public function saleinfo(){
+    public function saleinfo()
+    {
         return view('Other.saleinfo');
     }
 
-    public function apptest(){
-        return view('Other.apptest');
+    public function apptest()
+    {
+        $mobile = $this->isMobile();
+        return view('Other.apptest', ['mobile' => $mobile]);
     }
 
     public function orderlist()
@@ -120,12 +123,75 @@ class PageController extends ApiController
         return view('Other.orderlist');
     }
 
-    public function error404(){
+    public function error404()
+    {
         return view('errors.404');
     }
 
-    public function downapp(){
+    public function downapp()
+    {
         return View('designer.downapp');
+    }
+
+    private function isMobile()
+    {
+        if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) {
+            return true;
+        }
+
+        if (isset($_SERVER['HTTP_VIA'])) {
+            return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+        }
+
+        if (isset ($_SERVER['HTTP_USER_AGENT'])) {
+            $clientkeywords = array(
+                'nokia',
+                'sony',
+                'ericsson',
+                'mot',
+                'samsung',
+                'htc',
+                'sgh',
+                'lg',
+                'sharp',
+                'sie-',
+                'philips',
+                'panasonic',
+                'alcatel',
+                'lenovo',
+                'iphone',
+                'ipod',
+                'blackberry',
+                'meizu',
+                'android',
+                'netfront',
+                'symbian',
+                'ucweb',
+                'windowsce',
+                'palm',
+                'operamini',
+                'operamobi',
+                'openwave',
+                'nexusone',
+                'cldc',
+                'midp',
+                'wap',
+                'mobile'
+            );
+
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+                return true;
+            }
+        }
+
+        if (isset($_SERVER['HTTP_ACCEPT'])) {
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 }
 
