4b8de6e426aaa80837cd6cc2c4282d62e00a066b
SuperJu <juchao1989@foxmail.com>
2016-10-15 09:43:57 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -213,7 +213,8 @@ $app->get('/apptest', 'Other\PageController@apptest');
 
 //rae网红
 $app->get('/rae','Designer\DesignerController@skipDesigner');
-
+$app->get('/Rae','Designer\DesignerController@skipDesigner');
+$app->get('/RAE','Designer\DesignerController@skipDesigner');
 $app->get('/downapp','Other\PageController@downapp');
 
 $app->get('/orderlist', 'Other\PageController@orderlist');
