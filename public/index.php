c79f143d4f2079a8f24885260f0fdf552138ade7
SuperJu <juchao1989@foxmail.com>
2016-11-10 16:15:12 +0800

    update

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
-|
 | First we need to get an application instance. This creates an instance
 | of the application / container and bootstraps the application so it
 | is ready to receive HTTP / Console requests from the environment.
