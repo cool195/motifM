191b5bcae0dff27bd4f3e18327946cf7929e4c3c
zhangtao <250574087@qq.com>
2016-11-21 11:21:02 +0800

    商品详情页，增加商品分类字段

--- app/Http/Controllers/Shopping/ProductController.php
+++ app/Http/Controllers/Shopping/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Shopping;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends ApiController
 {
@@ -20,14 +21,33 @@ class ProductController extends ApiController
      * */
     public function index(Request $request, $spu)
     {
-        if (empty($spu)) {
-            return redirect('/shopping');
-        }
         $result = $this->getProductDetail($request, $spu);
-        if (!$result['success']) {
-            return redirect('/shopping');
+        if(!$result['success']){
+            abort(404);
+        } else {
+            $category = Cache::remember('category', 60, function () {
+                $params = array(
+                    'cmd' => 'categorylist',
+                );
+                return $this->request('openapi', self::API_SYSTEM, self::API_SERVICE, $params);
+            });
+            $categoryName = '';
+            foreach ($category['data']['list'] as $value) {
+                if ($value['category_id'] == $result['data']['front_category_ids'][0]) {
+                    $categoryName = $value['category_name'];
+                    $result['data']['category_id'] = $result['data']['front_category_ids'][0];
+                    break;
+                }
+            }
+            $result['data']['category_name'] = $categoryName;
         }
+        if ($request->input('ajax')){
+            return $result;
+        }
+
         $recommended = $this->recommended($spu,$result['data']['front_category_ids'][0],$result['data']['designer']['designer_id']);
+        error_log(print_r("------------------\n", "\n"), 3, '/tmp/myerror.log');
+        error_log(print_r($result, "\n"), 3, '/tmp/myerror.log');
         return View('shopping.detail', ['data' => $result['data'], 'recommended' => $recommended['data'],'NavShowShop'=>true]);
     }
 
