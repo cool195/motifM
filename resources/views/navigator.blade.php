5d5277534560fc7a51b06e2e1d0217be7fc595a7
zhangtao <250574087@qq.com>
2017-01-03 17:40:20 +0800

    see

--- resources/views/navigator.blade.php
+++ resources/views/navigator.blade.php
@@ -2,10 +2,10 @@
     <nav class="navbar navbar-full">
         <ul class="nav navbar-primary nav-top">
             <li class="nav-item nav-logo">
-                <H1>
                 <a href="/">
+                    <H1 style="line-height: 0;">
                     <img class="motif-logo" src="{{env('CDN_Static')}}/images/logo/logo.png" alt="Motif"
-                         srcset="{{env('CDN_Static')}}/images/logo/logo@2x.png 2x,{{env('CDN_Static')}}/images/logo/logo@3x.png 3x"></a></H1>
+                         srcset="{{env('CDN_Static')}}/images/logo/logo@2x.png 2x,{{env('CDN_Static')}}/images/logo/logo@3x.png 3x"></H1></a>
             </li>
             <li class="nav-item nav-hamburger">
                 <div class="nav-icon">
