2baaec151d542cdcf5ca5cd0b87fc3fb04145564
zhangtao <250574087@qq.com>
2017-02-04 18:35:14 +0800

    test

--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -183,7 +183,7 @@
                     </div>
                 @endif
 
-                @if($designer['designer_id']==114)
+                @if($designer['designer_id']==103)
                     <div class="font-size-sm text-primary p-y-15x p-x-15x">
                         <div class="text-center">
                             <div class="font-size-md">Follow Michaela to be notified when<br> this collection is available</div>
