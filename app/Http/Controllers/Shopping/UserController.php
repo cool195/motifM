009081d3272ce0b12335c0b8a34238389bce3b3a
zhangtao <250574087@qq.com>
2017-02-17 15:41:10 +0800

    去除断点

--- app/Http/Controllers/Shopping/UserController.php
+++ app/Http/Controllers/Shopping/UserController.php
@@ -140,8 +140,6 @@ class UserController extends ApiController
                 Publicfun::mergeCartSkus();
             }
         }
-        error_log(print_r("------------------\n", "\n"), 3, '/tmp/myerror.log');
-        error_log(print_r($result, "\n"), 3, '/tmp/myerror.log');
         return $result;
     }
 
