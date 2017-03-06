98fa8e2ff5624f25de335483c3904b1cf5aa126b
zhangtao <250574087@qq.com>
2017-03-06 19:26:08 +0800

    js css omg compress ,cdn update

--- vendor/laravel/lumen-framework/config/app.php
+++ vendor/laravel/lumen-framework/config/app.php
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'version' => 'v1.2.9',//.time(),
+    'version' => 'v1.3.0',//.time(),
     'key' => env('APP_KEY', 'SomeRandomString!!!'),
 
     'cipher' => 'AES-256-CBC',
