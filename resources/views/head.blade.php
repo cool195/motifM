8134c276ecf0f7452deeffb91c00748f60550daf
motif <dinghuihua@evermarker.net>
2017-03-08 19:12:47 +0800

    update   GA

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,12 +1,27 @@
 <meta charset="UTF-8">
 <meta name="keywords" content="Motif | fashion,style,shop,accessory,jewelry,watch,blogger,Instagram,designer,limited,edition,ecommerce,buy">
 <meta name="description" content="Motif | Your style is unique and cutting edge - your fashion should be too.Exclusive, limited edition accessories designed by the world’s top fashion bloggers, Instagrammers and digital influencers.">
-<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+{{--<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">--}}
+<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
 {{--<meta name="apple-itunes-app" content="app-id=1125850409">--}}
 <meta http-equiv="Access-Control-Allow-Origin" content="*">
 <link rel="icon" href="/favicon.ico">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="stylesheet" href="{{env('CDN_Static')}}/styles/vendor.css{{'?v='.config('app.version')}}">
+
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-78914929-6', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+
 <script src="{{env('CDN_Static')}}/scripts/vendor/modernizr.js{{'?v='.config('app.version')}}"></script>
 <script src="{{env('CDN_Static')}}/scripts/vendor/fastclick.js{{'?v='.config('app.version')}}"></script>
 <script>
